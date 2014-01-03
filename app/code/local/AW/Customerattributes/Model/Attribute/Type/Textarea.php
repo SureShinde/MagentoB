f19dd9734b8c8996a4158aec75aa12fde30b2115
darmayantiPuteri <darmayanti.puteri@gmail.com>
2014-01-03 18:54:09 +0700

    customer attributes ext

---
+++ app/code/local/AW/Customerattributes/Model/Attribute/Type/Textarea.php
@@ -0,0 +1,87 @@
+<?php
+/**
+ * aheadWorks Co.
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the EULA
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://ecommerce.aheadworks.com/AW-LICENSE.txt
+ *
+ * =================================================================
+ *                 MAGENTO EDITION USAGE NOTICE
+ * =================================================================
+ * This software is designed to work with Magento community edition and
+ * its use on an edition other than specified is prohibited. aheadWorks does not
+ * provide extension support in case of incorrect edition use.
+ * =================================================================
+ *
+ * @category   AW
+ * @package    AW_Customerattributes
+ * @version    1.0.2
+ * @copyright  Copyright (c) 2010-2012 aheadWorks Co. (http://www.aheadworks.com)
+ * @license    http://ecommerce.aheadworks.com/AW-LICENSE.txt
+ */
+
+
+class AW_Customerattributes_Model_Attribute_Type_Textarea extends AW_Customerattributes_Model_Attribute_TypeAbstract
+{
+    /**
+     * @return AW_Customerattributes_Block_Widget_Backend_Grid_Textarea
+     */
+    protected function _getBackendGridRenderer()
+    {
+        return new AW_Customerattributes_Block_Widget_Backend_Grid_Textarea();
+    }
+
+    /**
+     * @return AW_Customerattributes_Block_Widget_Backend_Form_Textarea
+     */
+    protected function _getBackendFormRenderer()
+    {
+        return new AW_Customerattributes_Block_Widget_Backend_Form_Textarea();
+    }
+
+    /**
+     * @return AW_Customerattributes_Block_Widget_Frontend_Form_Textarea
+     */
+    protected function _getFrontendFormRenderer()
+    {
+        return new AW_Customerattributes_Block_Widget_Frontend_Form_Textarea();
+    }
+
+    public function getValueType()
+    {
+        return AW_Customerattributes_Model_Resource_Value::TEXT_TYPE;
+    }
+
+    /**
+     * @param mixed $value
+     *
+     * @throws Exception
+     */
+    public function validate($value)
+    {
+        $helper = Mage::helper('aw_customerattributes');
+        $storeId = Mage::app()->getStore()->getId();
+        $label = $this->getAttribute()->getLabel($storeId);
+        $label = Mage::helper('core')->escapeHtml($label);
+        if (strlen(trim($value)) < 1) {
+            if ($this->getAttribute()->getData('validation_rules/is_required')) {
+                throw new Exception($helper->__('%s is required', $label));
+            }
+        }
+        if ($length = $this->getAttribute()->getData('validation_rules/minimum_text_length')) {
+            if (!Zend_Validate::is($value, 'StringLength', array('min' => $length, 'max' => null))) {
+                throw new Exception($helper->__("%s is less than '%s' symbols", $label, $length));
+            }
+        }
+        if ($length = $this->getAttribute()->getData('validation_rules/maximum_text_length')) {
+            if (!Zend_Validate::is($value, 'StringLength', array('min' => 0, 'max' => $length))) {
+                throw new Exception($helper->__("%s is greater than '%s' symbols", $label, $length));
+            }
+        }
+        return;
+    }
+}
