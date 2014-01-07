dbc7ad46755a2198ec44cd38ce685c1f8a494d0c
farsilosa <farsi@bilna.com>
2014-01-07 10:26:09 +0700

    re #569 deals website

--- app/code/local/Bilna/Newpage/Block/Index.php
+++
@@ -1,8 +0,0 @@
-<?php   
-class Bilna_Newpage_Block_Index extends Mage_Core_Block_Template{   
-
-
-
-
-
-}
--- app/code/local/Bilna/Newpage/Helper/Data.php
+++
@@ -1,5 +0,0 @@
-<?php
-class Bilna_Newpage_Helper_Data extends Mage_Core_Helper_Abstract
-{
-}
-	 
