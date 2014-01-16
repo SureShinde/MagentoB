85ffa32a73c9628ab35b3aed35e0fd4f6b38bc76
Dhany Alfian <dhany@bilna.com>
2014-01-16 10:38:23 +0700

    re #583 | VTDirect: Support Cicilan

--- app/code/local/Bilna/Paymethod/Block/Vtdirect/Thankyou.php
+++ app/code/local/Bilna/Paymethod/Block/Vtdirect/Thankyou.php
@@ -9,30 +9,4 @@ class Bilna_Paymethod_Block_Vtdirect_Thankyou extends Mage_Core_Block_Template {
     public function getResponseCharge() {
         return Mage::registry('response_charge');
     }
-//    public function getOrderId() {
-//        return Mage::helper('paymethod/vtdirect')->getOrderId();
-//    }
-//    
-//    public function getOrder() {
-//        return Mage::getModel('sales/order')->load($this->getOrderId());
-//    }
-//    
-//    public function getTokenId() {
-//        //Mage::getSingleton('core/session')->setVtdirectTokenIdCreate(date('yyyy-mm-dd H:i:s'));
-//        //Mage::getSingleton('core/session')->setVtdirectTokenId($data['token_id']);
-//        
-//        return Mage::getSingleton('core/session')->getVtdirectTokenId();
-//    }
-//    
-//    public function getChargeUrl() {
-//        return $this->getBaseUrl() . 'paymethod/vtdirect/payment/';
-//    }
-//    
-//    public function maxChar($text, $maxLength = 10) {
-//        return substr($text, 0, $maxLength);
-//    }
-//    
-//    public function removeSymbols($text) {
-//        return Mage::helper('paymethod/vtdirect')->removeSymbols($text);
-//    }
 }
