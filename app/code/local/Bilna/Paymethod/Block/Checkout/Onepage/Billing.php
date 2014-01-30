f54d0932304c98a47ee3fb9b1ffb8edbd01a4b7c
Dhany Alfian <dhany@bilna.com>
2014-01-30 14:08:41 +0700

    re #657 - Order - HTTPS Checkout - fix bugs change http to https

--- app/code/local/Bilna/Paymethod/Block/Checkout/Onepage/Billing.php
+++ app/code/local/Bilna/Paymethod/Block/Checkout/Onepage/Billing.php
@@ -1,11 +1,11 @@
 <?php
 /**
- * Description of Bilna_Paymethod_Block_Checkout_Onepage
+ * Description of Bilna_Paymethod_Block_Checkout_Onepage_Billing
  *
  * @author Bilna Development Team <development@bilna.com>
  */
 
-class Bilna_Paymethod_Block_Checkout_Onepage extends Mage_Checkout_Block_Onepage {
+class Bilna_Paymethod_Block_Checkout_Onepage_Billing extends Mage_Checkout_Block_Onepage_Billing {
     public function getUrl($route = '', $params = array ()) {
         $url = Mage::helper('paymethod')->checkHttpsProtocol($this->_getUrlModel()->getUrl($route, $params));
         
--- app/code/local/Bilna/Paymethod/Helper/Data.php
+++ app/code/local/Bilna/Paymethod/Helper/Data.php
@@ -154,4 +154,20 @@ class Bilna_Paymethod_Helper_Data extends Mage_Core_Helper_Abstract {
         
         return false;
     }
+    
+    public function checkHttpsProtocol($url) {
+        $isSecure = false;
+        
+        if (isset ($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on') {
+            $isSecure = true;
+        }
+        else if (!empty ($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https' || !empty ($_SERVER['HTTP_X_FORWARDED_SSL']) && $_SERVER['HTTP_X_FORWARDED_SSL'] == 'on') {
+            $isSecure = true;
+        }
+        
+        $REQUEST_PROTOCOL = $isSecure ? 'https' : 'http';
+        $result = str_replace('http', $REQUEST_PROTOCOL, $url);
+        
+        return $result;
+    }
 }
