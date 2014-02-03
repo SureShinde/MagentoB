99be22097362845693089aa2f7d50adc92d606b5
Dhany Alfian <dhany@bilna.com>
2014-02-03 16:40:25 +0700

    - remove ANZ Credit Card Payment Method

--- app/code/local/Bilna/Anzcc/sql/anzcc_setup/mysql4-install-0.1.0.php
+++
@@ -1,8 +0,0 @@
-<?php
-$installer = $this;
-$installer->startSetup();
-$installer->run("
-    ALTER TABLE `{$installer->getTable('sales/quote_payment')}` ADD `anzcc_bins` VARCHAR(6) DEFAULT NULL;
-    ALTER TABLE `{$installer->getTable('sales/order_payment')}` ADD `anzcc_bins` VARCHAR(6) DEFAULT NULL;
-");
-$installer->endSetup();
