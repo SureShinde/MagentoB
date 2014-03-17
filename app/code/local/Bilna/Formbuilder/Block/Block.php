f69188b3101f27c979c41c47887d75dc6fadbe63
Richi Prathama <richie@bilna.com>
2014-03-17 15:46:04 +0700

    BILNA-106 #display formbuilder at frontend

--- app/code/local/Bilna/Formbuilder/Block/Block.php
+++ app/code/local/Bilna/Formbuilder/Block/Block.php
@@ -8,33 +8,28 @@ class Bilna_Formbuilder_Block_Block extends Mage_Core_Block_Template
     public function __construct()
     {
         parent::__construct();
+        $this->setTemplate('formbuilder/form/default.phtml');
     }
     
     protected function _toHtml($status=false)
     {
-    	$block = NULL;
+    	$this->block = NULL;
         $blockId = $this->getBlockId();
-        $html = '';
         if ($blockId) {
-			$block = Mage::getModel('bilna_formbuilder/formbuilder')->getCollection();
-			$block->getSelect()->join('bilna_formbuilder_input', 'main_table.id = bilna_formbuilder_input.form_id');
-	                //->setStoreId(Mage::app()->getStore()->getId())
-	                //->load($blockId);
-// 			$block->printLogQuery(true);die;
-			$this->setCollection($block);
+			$this->block = Mage::getModel('bilna_formbuilder/formbuilder')->getCollection();
+			$this->block->getSelect()->join('bilna_formbuilder_input', 'main_table.id = bilna_formbuilder_input.form_id');
+// 	                //->setStoreId(Mage::app()->getStore()->getId())
+// 	                //->load($blockId);
+// // 			$block->printLogQuery(true);die;
+			$this->setCollection($this->block);
 			
-// 	        if ($block->getStatus()) {
-	        	/* @var $helper Mage_Cms_Helper_Data */
-// 	        	$helper = Mage::helper('bilna_formbuilder');
-				//$processor = $helper->getBlockTemplateProcessor();
-// 	        }
+// // 	        if ($block->getStatus()) {
+// 	        	/* @var $helper Mage_Cms_Helper_Data */
+// // 	        	$helper = Mage::helper('bilna_formbuilder');
+// 				//$processor = $helper->getBlockTemplateProcessor();
+// // 	        }
         }
         
-        $this->setTemplate('formbuilder/form/default.phtml');
-        return $this->_toHtml(true);
-        if (!$this->getTemplate()) {
-            return '';
-        }
         $html = $this->renderView();
         return $html;
     }
