8374de76fe632fe7d1a16fcd9f0fb511b0362395
Richi Prathama <richie@bilna.com>
2014-03-17 12:20:30 +0700

    BILNA-106 #Modify form builder with phtml

--- app/code/local/Bilna/Formbuilder/Block/Block.php
+++ app/code/local/Bilna/Formbuilder/Block/Block.php
@@ -7,43 +7,35 @@ class Bilna_Formbuilder_Block_Block extends Mage_Core_Block_Template
 {
     public function __construct()
     {
-echo 5;die;
         parent::__construct();
-        $this->setTemplate('formbuilder/form/default.phtml');
     }
-
-    /**
-     * Prepare Content HTML
-     *
-     * @return string
-     */
-    protected function _toHtml()
+    
+    protected function _toHtml($status=false)
     {
-	$block = NULL;
+    	$block = NULL;
         $blockId = $this->getBlockId();
         $html = '';
         if ($blockId) {
-		$block = Mage::getModel('bilna_formbuilder/formbuilder')->getCollection();
-		$block->getSelect()->join('bilna_formbuilder_input', 'main_table.id = bilna_formbuilder_input.form_id');
-                //->setStoreId(Mage::app()->getStore()->getId())
-                //->load($blockId);
-		$this->setCollection($block);
-		//$block->printLogQuery(true);
-		//Zend_Debug::Dump($block); die;
-                //if ($block->getStatus()) {
-                /* @var $helper Mage_Cms_Helper_Data */
-                $helper = Mage::helper('bilna_formbuilder');
-		//Zend_Debug::Dump($block); die;
-		//$processor = $helper->getBlockTemplateProcessor();
-                //$html = 'input';
-	//foreach($block as $row) {
-	 	//echo $row->getTitle().":";
-		//echo "<br/>";
-	//}
-                //$this->addModelTags($block);
-		//}
+			$block = Mage::getModel('bilna_formbuilder/formbuilder')->getCollection();
+			$block->getSelect()->join('bilna_formbuilder_input', 'main_table.id = bilna_formbuilder_input.form_id');
+	                //->setStoreId(Mage::app()->getStore()->getId())
+	                //->load($blockId);
+// 			$block->printLogQuery(true);die;
+			$this->setCollection($block);
+			
+// 	        if ($block->getStatus()) {
+	        	/* @var $helper Mage_Cms_Helper_Data */
+// 	        	$helper = Mage::helper('bilna_formbuilder');
+				//$processor = $helper->getBlockTemplateProcessor();
+// 	        }
         }
-	$this->renderLayout();
-        return $block;
+        
+        $this->setTemplate('formbuilder/form/default.phtml');
+        return $this->_toHtml(true);
+        if (!$this->getTemplate()) {
+            return '';
+        }
+        $html = $this->renderView();
+        return $html;
     }
 }
