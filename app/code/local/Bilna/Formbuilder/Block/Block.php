35bd121da058f95b55f4b67984d89cf11b9e8528
Andhi Irawan <andi@bilna.com>
2014-03-18 11:11:38 +0700

    BILNA-106 #Modify Form Builder

--- app/code/local/Bilna/Formbuilder/Block/Block.php
+++ app/code/local/Bilna/Formbuilder/Block/Block.php
@@ -14,12 +14,12 @@ class Bilna_Formbuilder_Block_Block extends Mage_Core_Block_Template
     protected function _toHtml($status=false)
     {
     	$this->block = NULL;
-        $blockId = $this->getBlockId();
-        if ($blockId) {
+        $this->blockId = $this->getBlockId();
+        if ($this->blockId) {
 			$this->block = Mage::getModel('bilna_formbuilder/formbuilder')->getCollection();
 			$this->block->getSelect()->join('bilna_formbuilder_input', 'main_table.id = bilna_formbuilder_input.form_id');
 // 	                //->setStoreId(Mage::app()->getStore()->getId())
-// 	                //->load($blockId);
+// 	                //->load($this->blockId);
 // // 			$block->printLogQuery(true);die;
 			$this->setCollection($this->block);
 			
