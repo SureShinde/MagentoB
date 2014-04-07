c433a8422b74dc9fccb30706eb165521c408126c
Richi Prathama <richie@bilna.com>
2014-04-07 15:35:15 +0700

    BILNA-166 #Fix email how to sent the variable

--- app/code/local/Bilna/Formbuilder/Block/Block.php
+++ app/code/local/Bilna/Formbuilder/Block/Block.php
@@ -13,22 +13,22 @@ class Bilna_Formbuilder_Block_Block extends Mage_Core_Block_Template
     {
     	$this->block = NULL;
     	$this->inputs = NULL;
-      $this->blockId = $this->getBlockId();
+		$this->blockId = $this->getBlockId();
 
-      if ($this->blockId) {
-				$this->block = Mage::getModel('bilna_formbuilder/form')->getCollection();
-				$this->block->getSelect();
-				$this->block->addFieldToFilter('main_table.id', $this->blockId);
-				$this->block = $this->block->getFirstItem();
-			
-				$this->inputs = Mage::getModel('bilna_formbuilder/form')->getCollection();
-				$this->inputs->getSelect()->join('bilna_formbuilder_input', 'main_table.id = bilna_formbuilder_input.form_id');
-				$this->inputs->addFieldToFilter('main_table.id', $this->blockId)
-							->addOrder('bilna_formbuilder_input.order', 'ASC')
-							->addOrder('bilna_formbuilder_input.group', 'ASC');
-      }
+		if ($this->blockId) {
+			$this->block = Mage::getModel('bilna_formbuilder/form')->getCollection();
+			$this->block->getSelect();
+			$this->block->addFieldToFilter('main_table.id', $this->blockId);
+			$this->block = $this->block->getFirstItem();
+		
+			$this->inputs = Mage::getModel('bilna_formbuilder/form')->getCollection();
+			$this->inputs->getSelect()->join('bilna_formbuilder_input', 'main_table.id = bilna_formbuilder_input.form_id');
+			$this->inputs->addFieldToFilter('main_table.id', $this->blockId)
+						->addOrder('bilna_formbuilder_input.order', 'ASC')
+						->addOrder('bilna_formbuilder_input.group', 'ASC');
+		}
 
-        $html = $this->renderView();
-        return $html;
-    }
+		$html = $this->renderView();
+		return $html;
+	}
 }
