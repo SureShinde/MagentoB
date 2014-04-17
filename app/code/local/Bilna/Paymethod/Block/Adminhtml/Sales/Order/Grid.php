469a1c6adeefb0259bae2367cd5261c2530545ce
Bilna Web Administrator <webadmin@admin1.bilna.com>
2014-04-17 03:44:08 +0000

    BILNA-311 #Error in the admin page and Order yang Hilang

--- app/code/local/Bilna/Paymethod/Block/Adminhtml/Sales/Order/Grid.php
+++ app/code/local/Bilna/Paymethod/Block/Adminhtml/Sales/Order/Grid.php
@@ -26,13 +26,15 @@ class Bilna_Paymethod_Block_Adminhtml_Sales_Order_Grid extends Mage_Adminhtml_Bl
     
     protected function _prepareCollection() {
         $collection = Mage::getResourceModel($this->_getCollectionClass());
-        $collection->join(array ('payment'=>'sales/order_payment'),'main_table.entity_id=parent_id','method');
+		$collection->join(array ('payment' => 'sales/order_payment'), 'main_table.entity_id = parent_id', 'method');
         $collection->addFilterToMap('increment_id', 'main_table.increment_id');
-        $collection->join(array('customer'=>'customer/entity'),
-            'main_table.customer_id=customer.entity_id',
-            array(
-                'group_id' => 'customer.group_id'
-            )
+        $collection->addFilterToMap('store_id', 'main_table.store_id');
+        $collection->addFilterToMap('created_at', 'main_table.created_at');
+        $collection->addFilterToMap('group_id', 'customer_entity.group_id');
+        $collection->getSelect()->joinLeft(
+            array ('customer_entity'),
+            'main_table.customer_id = customer_entity.entity_id',
+            array ('group_id' => 'customer_entity.group_id')
         );
         $this->setCollection($collection);
 
