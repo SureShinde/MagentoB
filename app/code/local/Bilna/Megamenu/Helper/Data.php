7a8f37967875a6d65a002ac3ab5ac507bd4f9f5d
Dhany Alfian <dhany@bilna.com>
2014-04-23 15:04:37 +0700

    BILNA-312 #Modify Body Html to show current parent category - change category class name with url key

--- app/code/local/Bilna/Megamenu/Helper/Data.php
+++ app/code/local/Bilna/Megamenu/Helper/Data.php
@@ -15,30 +15,48 @@ class Bilna_Megamenu_Helper_Data extends Mage_Catalog_Helper_Category {
     }
     
     public function getCurrentMainCategory() {
-        $mainCategory = array ('baby', 'home', 'daily-deals');
+        $mainCategory = $this->getMainCategory();
         $result = '';
         
         if ($categoryId = $this->getCurrentCategoryFrontend()) {
-            if (in_array($this->getCategoryName($categoryId), $mainCategory)) {
-                $result = $this->getCategoryName($categoryId);
+            if (in_array(Mage::getModel('catalog/category')->load($categoryId)->getUrlKey(), $mainCategory)) {
+                $result = $categoryId;
             }
             
-            if (in_array($this->getCategoryName(Mage::getModel('catalog/category')->load($categoryId)->getParentId()), $mainCategory)) {
-                $result = $this->getCategoryName(Mage::getModel('catalog/category')->load($categoryId)->getParentId());
+            if (in_array($this->getUrlKey(Mage::getModel('catalog/category')->load($categoryId)->getParentId()), $mainCategory)) {
+                $result = Mage::getModel('catalog/category')->load($categoryId)->getParentId();
             }
             
-            if (in_array($this->getCategoryName(Mage::getModel('catalog/category')->load(Mage::getModel('catalog/category')->load($categoryId)->getParentId())->getParentId()), $mainCategory)) {
-                $result = $this->getCategoryName(Mage::getModel('catalog/category')->load(Mage::getModel('catalog/category')->load($categoryId)->getParentId())->getParentId());
+            if (in_array($this->getUrlKey(Mage::getModel('catalog/category')->load(Mage::getModel('catalog/category')->load($categoryId)->getParentId())->getParentId()), $mainCategory)) {
+                $result = Mage::getModel('catalog/category')->load(Mage::getModel('catalog/category')->load($categoryId)->getParentId())->getParentId();
             }
         }
         
         return $result;
     }
     
+    protected function getMainCategory() {
+        $categories = $this->getStoreCategories();
+        $result = array ();
+        
+        if (count($categories) > 0) {
+            foreach ($categories as $category) {
+                $categoryData = Mage::getModel('catalog/category')->load($category->getId());
+                $result[] = $categoryData->getUrlKey();
+            }
+        }
+        
+        return $result;
+    }
+    
+    protected function getUrlKey($categoryId) {
+        return Mage::getModel('catalog/category')->load($categoryId)->getUrlKey();
+    }
+    
     public function getCategoryName($categoryId) {
         return strtolower(str_replace(array (' & ', ' ', '!'), array ('-', '-', ''), Mage::getModel('catalog/category')->load($categoryId)->getName()));
     }
-    
+
     public function replaceCategoryName($categoryName) {
         return str_replace('&', '<br/>&', $categoryName);
     }
