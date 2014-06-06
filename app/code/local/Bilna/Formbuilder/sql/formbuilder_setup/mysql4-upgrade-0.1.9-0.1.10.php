3267cbe4de4d7cc5f9badd1ca712bc51ccb179cc
Richi Prathama <richie@bilna.com>
2014-06-06 11:48:27 +0700

    BILNA-475 #add termsconditions

--- app/code/local/Bilna/Formbuilder/sql/formbuilder_setup/mysql4-upgrade-0.1.9-0.1.10.php
+++ app/code/local/Bilna/Formbuilder/sql/formbuilder_setup/mysql4-upgrade-0.1.9-0.1.10.php
@@ -6,8 +6,11 @@ $installer->startSetup();
 
 $installer->run("
 
-ALTER TABLE `bilna_formbuilder_form` ADD COLUMN `class` VARCHAR(128) NULL  AFTER `active_to` ;
-ALTER TABLE `bilna_formbuilder_form` ADD COLUMN `button_text` VARCHAR(32) NULL  AFTER `class` ;
+ALTER TABLE `bilna_formbuilder_form` 
+	ADD COLUMN `class` VARCHAR(128) NULL  AFTER `active_to` ,
+	ADD COLUMN `button_text` VARCHAR(32) NULL  AFTER `class` ,
+	ADD COLUMN `termsconditions` TEXT NULL  AFTER `url` , 
+	ADD COLUMN `freeproducts` VARCHAR(256) NULL  AFTER `termsconditions` ;
 
 ");
 
