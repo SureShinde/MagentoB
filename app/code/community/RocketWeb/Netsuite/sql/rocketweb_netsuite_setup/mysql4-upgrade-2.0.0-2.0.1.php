c93ddb8921f05419f7eb5023cb1937a2b3adb94e
Dhany Alfian <dhany@bilna.com>
2014-06-09 10:57:42 +0700

    BILNA-300 #Netsuite - Expected and Event Cost - add attribute expected_cost, event_cost, event_start_date, event_end_date from sql setup file

---
+++ app/code/community/RocketWeb/Netsuite/sql/rocketweb_netsuite_setup/mysql4-upgrade-2.0.0-2.0.1.php
@@ -0,0 +1,43 @@
+<?php
+$installer = $this;
+$installer->startSetup();
+
+$setup = new Mage_Eav_Model_Entity_Setup('core_setup');
+$setup->addAttribute('catalog_product', 'expected_cost', array (
+    'type' => 'float',
+    'label' => 'Expected Cost',
+    'input' => 'text',
+    'sort_order' => 200,
+    'global' => Mage_Catalog_Model_Resource_Eav_Attribute::SCOPE_STORE,
+    'group' => 'General',
+    'required' => false
+));
+$setup->addAttribute('catalog_product', 'event_cost', array (
+    'type' => 'float',
+    'label' => 'Event Cost',
+    'input' => 'text',
+    'sort_order' => 210,
+    'global' => Mage_Catalog_Model_Resource_Eav_Attribute::SCOPE_STORE,
+    'group' => 'General',
+    'required' => false
+));
+$setup->addAttribute('catalog_product', 'event_start_date', array (
+    'type' => 'date',
+    'label' => 'Event Start Date',
+    'input' => 'date',
+    'sort_order' => 220,
+    'global' => Mage_Catalog_Model_Resource_Eav_Attribute::SCOPE_STORE,
+    'group' => 'General',
+    'required' => false
+));
+$setup->addAttribute('catalog_product', 'event_end_date', array (
+    'type' => 'date',
+    'label' => 'Event End Date',
+    'input' => 'date',
+    'sort_order' => 230,
+    'global' => Mage_Catalog_Model_Resource_Eav_Attribute::SCOPE_STORE,
+    'group' => 'General',
+    'required' => false
+));
+
+$installer->endSetup();
