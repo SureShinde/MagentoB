05bd365ac287406be278bfd43d87b7ebf8632a95
Dhany Alfian <dhany@bilna.com>
2014-08-18 10:19:30 +0700

    BILNA-657 #NS - Log Shipping Error & Delete from queue - save format encoding Netbeans Linux

--- app/code/community/RocketWeb/Netsuite/Helper/Mapper/Shipment.php
+++ app/code/community/RocketWeb/Netsuite/Helper/Mapper/Shipment.php
@@ -16,69 +16,6 @@
  */
 
 class RocketWeb_Netsuite_Helper_Mapper_Shipment extends RocketWeb_Netsuite_Helper_Mapper {
-    public function getMagentoFormatOld(ItemFulfillment $netsuiteShipment) {
-        $magentoShipment = Mage::getModel('sales/order_shipment');
-        $netsuiteOrderId = $netsuiteShipment->createdFrom->internalId;
-        $magentoOrders = Mage::getModel('sales/order')->getCollection()->addFieldToFilter('netsuite_internal_id', $netsuiteOrderId);
-        $magentoOrder = $magentoOrders->getFirstItem(); // @var Mage_Sales_Model_Order $magentoOrder
-
-        if (!is_object($magentoOrder) || !$magentoOrder->getId()) {
-            throw new Exception("Order with netsuite internal id {$netsuiteShipment->createdFrom->internalId} not found in Magento!");
-        }
-
-        $netsuiteCustomer = Mage::helper('rocketweb_netsuite/mapper_customer')->getByInternalId($netsuiteShipment->entity->internalId);
-        $magentoShipment->setStoreId($magentoOrder->getStoreId());
-        $magentoShipment->setCustomerId($magentoOrder->getCustomerId()); //we assume customer is not changed for shipping
-        $magentoShipment->setBillingAddressId($magentoOrder->getBillingAddressId()); //billing address is not part of a netsuite fulfillment, use the one in Magento
-        $magentoShippingAddress = Mage::helper('rocketweb_netsuite/mapper_address')->getShippingAddressMagentoFormatFromNetsuiteAddress($netsuiteShipment->transactionShipAddress, $netsuiteCustomer, $magentoOrder);
-        $magentoShippingAddress->setId($magentoOrder->getShippingAddressId());
-        $magentoShippingAddress->save();
-
-        Mage::helper('core')->copyFieldset('sales_convert_order', 'to_shipment', $magentoOrder, $magentoShipment);
-        $magentoShipment->setShippingAddressId($magentoShippingAddress->getId());
-        $magentoShipment->setOrderId($magentoOrder->getId());
-        $magentoShipment->setCustomerId($magentoOrder->getCustomerId());
-        $shipmentMap = array ();
-
-        foreach ($netsuiteShipment->itemList->item as $netsuiteShipmentItem) {
-            foreach ($magentoOrder->getAllItems() as $magentoOrderItem) {
-                if ($netsuiteShipmentItem->item->name == $magentoOrderItem->getName()) {
-                    $shipmentMapItem = array ();
-                    $shipmentMapItem['netsuite_object'] = $netsuiteShipmentItem;
-                    $shipmentMapItem['magento_orderitem_object'] = $magentoOrderItem;
-                    $shipmentMap[] = $shipmentMapItem;
-                }
-            }
-        }
-
-        $totalQuantity = 0;
-
-        if (is_array($shipmentMap)) {
-            foreach ($shipmentMap as $shipmentMapItem) {
-                $magentoShipmentItem = Mage::getModel('sales/order_shipment_item');
-                Mage::helper('core')->copyFieldset('sales_convert_order_item', 'to_shipment_item', $shipmentMapItem['magento_orderitem_object'], $magentoShipmentItem);
-                $magentoShipmentItem->setOrderItem($shipmentMapItem['magento_orderitem_object']);
-                $magentoShipmentItem->setProductId(Mage::getModel('catalog/product')->load($shipmentMapItem['magento_orderitem_object']->getProductId())->getId());
-                $magentoShipmentItem->setQty($shipmentMapItem['netsuite_object']->quantity);
-                $magentoShipmentItem->getOrderItem()->setQtyShipped($shipmentMapItem['netsuite_object']->quantity);
-                $magentoShipment->addItem($magentoShipmentItem);
-                $totalQuantity += $shipmentMapItem['netsuite_object']->quantity;
-            }
-        }
-
-        $magentoShipment->setTotalQty($totalQuantity);
-        $trackingNumbers = Mage::helper('rocketweb_netsuite/mapper_trackingnumber')->getNormalizedTrackingNumberData($netsuiteShipment);
-
-        if (count($trackingNumbers)) {
-            foreach ($trackingNumbers as $trackingNumberData) {
-                $magentoTrackingNumber = Mage::helper('rocketweb_netsuite/mapper_trackingnumber')->getMagentoFormat($trackingNumberData, $netsuiteShipment->shipMethod);
-                $magentoShipment->addTrack($magentoTrackingNumber);
-            }
-        }
-        
-        return $magentoShipment;
-    }
-    
     public function getMagentoFormat(ItemFulfillment $netsuiteShipment, $queueData) {
         $lastModifiedDate = Mage::helper('rocketweb_netsuite')->convertNetsuiteDateToSqlFormat($netsuiteShipment->lastModifiedDate);
         $netsuiteShipmentInternalId = $netsuiteShipment->internalId;
