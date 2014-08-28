cc43b1e0b073a426777089f02438bb09f410704f
Dhany Alfian <dhany@bilna.com>
2014-08-28 12:18:17 +0700

    BILNA-721 #Request : Payment Method for UOB Creadit Card

---
+++ app/code/local/Bilna/Paymethod/Model/Method/Uobvisa.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Description of Bilna_Paymethod_Model_Method_Uobvisa
+ *
+ * @author Bilna Development Team <development@bilna.com>
+ */
+
+class Bilna_Paymethod_Model_Method_Uobvisa extends Bilna_Paymethod_Model_Method_Vtdirect {
+    protected $_code = 'uobvisa';
+}
