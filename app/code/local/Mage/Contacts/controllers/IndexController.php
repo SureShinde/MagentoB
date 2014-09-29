91fb09a4e739503c16349014b6ce07c006875cb0
Andhi Irawan <andi@bilna.com>
2014-09-29 16:46:55 +0700

    BILNA-718 #Contact Us Form Multi Email Recipient

--- app/code/local/Mage/Contacts/controllers/IndexController.php
+++ app/code/local/Mage/Contacts/controllers/IndexController.php
@@ -1,7 +1,6 @@
 <?php
 class Mage_Contacts_IndexController extends Mage_Core_Controller_Front_Action
 {
-
     const XML_PATH_EMAIL_RECIPIENT  = 'contacts/email/recipient_email';
     const XML_PATH_EMAIL_SENDER     = 'contacts/email/sender_email_identity';
     const XML_PATH_EMAIL_TEMPLATE   = 'contacts/email/email_template';
@@ -59,20 +58,25 @@ class Mage_Contacts_IndexController extends Mage_Core_Controller_Front_Action
                 if ($error) {
                     throw new Exception();
                 }
+                
                 $mailTemplate = Mage::getModel('core/email_template');
                 /* @var $mailTemplate Mage_Core_Model_Email_Template */
-                $mailTemplate->setDesignConfig(array('area' => 'frontend'))
+
+                $recipients = explode(",",Mage::getStoreConfig(self::XML_PATH_EMAIL_RECIPIENT));
+                foreach($recipients as $recipient){
+                    $mailTemplate->setDesignConfig(array('area' => 'frontend'))
                     ->setReplyTo($post['email'])
                     ->sendTransactional(
-                        Mage::getStoreConfig(self::XML_PATH_EMAIL_TEMPLATE),
-                        Mage::getStoreConfig(self::XML_PATH_EMAIL_SENDER),
-                        Mage::getStoreConfig(self::XML_PATH_EMAIL_RECIPIENT),
-                        null,
-                        array('data' => $postObject)
+                    Mage::getStoreConfig(self::XML_PATH_EMAIL_TEMPLATE),
+                    Mage::getStoreConfig(self::XML_PATH_EMAIL_SENDER),
+                    //$recipient,
+                    null,
+                    array('data' => $postObject)
                     );
-
-                if (!$mailTemplate->getSentSuccess()) {
-                    throw new Exception();
+                     
+                    if (!$mailTemplate->getSentSuccess()) {
+                        throw new Exception();
+                    }
                 }
 
                 $translate->setTranslateInline(true);
@@ -80,14 +84,17 @@ class Mage_Contacts_IndexController extends Mage_Core_Controller_Front_Action
                 Mage::getSingleton('customer/session')->addSuccess(Mage::helper('contacts')->__('Your inquiry was submitted and will be responded to as soon as possible. Thank you for contacting us.'));
                 $this->_redirect('*/*/');
 
-                return;
-            } catch (Exception $e) {
-                $translate->setTranslateInline(true);
-
-                Mage::getSingleton('customer/session')->addError(Mage::helper('contacts')->__('Unable to submit your request. Please, try again later'));
+                Mage::getSingleton('customer/session')->addSuccess(Mage::helper('contacts')->__('Your inquiry was submitted and will be responded to as soon as possible. Thank you for contacting us.'));
                 $this->_redirect('*/*/');
+
                 return;
-            }
+                } catch (Exception $e) {
+                    $translate->setTranslateInline(true);
+
+                    Mage::getSingleton('customer/session')->addError(Mage::helper('contacts')->__('Unable to submit your request. Please, try again later'));
+                    $this->_redirect('*/*/');
+                    return;
+                }
 
         } else {
             $this->_redirect('*/*/');
