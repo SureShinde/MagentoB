6c246901d4f4a9af85d2242a64bcfd55ccb129dd
farsilosa <farsi@bilna.com>
2014-10-13 19:05:45 +0700

    BILNA-809 #add left mobile menu

--- skin/frontend/base/default/js/bilna-lazyload.js
+++ skin/frontend/base/default/js/bilna-lazyload.js
@@ -1,10 +1,9 @@
-$j(document).ready(function(){
+$j(window).load(function(){
  var donkey_lazy = function() {
   $j('.lazy').each(function(){
    var scrollBottom = $j(window).scrollTop() + $j(window).height();
    
-   var pos = $j(this).position();
-   
+   var pos = $j(this).offset();
    if(scrollBottom >= pos.top){
    
     $j(this).addClass("donkey-image");
