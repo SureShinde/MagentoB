4c0f64beadbe0a7a8a9907d5b2189732e4bd14be
Richi Prathama <richie@bilna.com>
2014-10-20 20:49:10 +0700

    dirty code

--- skin/frontend/base/default/js/bilna-lazyload.js
+++ skin/frontend/base/default/js/bilna-lazyload.js
@@ -1,26 +1,4 @@
 $j(window).load(function(){
- var donkey_lazy = function() {
-  $j('.lazy').each(function(){
-   var scrollBottom = $j(window).scrollTop() + $j(window).height();
-   
-   var pos = $j(this).offset();
-   if(scrollBottom >= pos.top){
-   
-    $j(this).addClass("donkey-image");
-    $j(this).removeClass("lazy");
-    
-    $j(this).attr('src', $j(this).data('src'));
-   }
-  });
- };
- 
- donkey_lazy();
- 
- $j(window).scroll(function(){
-  donkey_lazy();
- });
- 
-
  var donkey_check_menuactive = function() {
 	if($j( ".web-perlengkapan-bayi" ).hasClass( "active" )){
 		$j( ".nav-baby" ).removeClass( "hidden" )
@@ -41,4 +19,25 @@ $j(window).load(function(){
  };
  
  donkey_check_menuactive();
+ 
+ var donkey_lazy = function() {
+  $j('.lazy').each(function(){
+   var scrollBottom = $j(window).scrollTop() + $j(window).height();
+   
+   var pos = $j(this).offset();
+   if(scrollBottom >= pos.top){
+   
+    $j(this).addClass("donkey-image");
+    $j(this).removeClass("lazy");
+    
+    $j(this).attr('src', $j(this).data('src'));
+   }
+  });
+ };
+ 
+ donkey_lazy();
+ 
+ $j(window).scroll(function(){
+  donkey_lazy();
+ });
 });
