fb24106556b04273990c3e9c697547adfcc1a759
Taufik Ramdani <taufik.r@bilna.com>
2015-03-03 17:33:14 +0700

    merge unknown files

---
+++ app/code/local/Bilna/Paymethod/Model/Method/Mandiridebitvisa.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Description of Bilna_Paymethod_Model_Method_Mandiridebitvisa
+ *
+ * @author Bilna Development Team <development@bilna.com>
+ */
+
+class Bilna_Paymethod_Model_Method_Mandiridebitvisa extends Bilna_Paymethod_Model_Method_Vtdirect {
+    protected $_code = 'mandiridebitvisa';
+}
