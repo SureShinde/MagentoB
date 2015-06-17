0a42de99719512dd8f651c1f77c4016a02605057
Taufik Ramdani <taufik.r@bilna.com>
2015-06-17 17:07:51 +0700

    fixing bilna credit calculation

--- app/code/community/RocketWeb/Netsuite/Helper/Mapper/Order.php
+++ app/code/community/RocketWeb/Netsuite/Helper/Mapper/Order.php
@@ -212,6 +212,25 @@ class RocketWeb_Netsuite_Helper_Mapper_Order extends RocketWeb_Netsuite_Helper_M
                     }
                 }
 
+                /**/
+                $bundleDiscount = 0;
+                $discountPerItem = 0;
+                if( (isset( $productOptions['bundle_selection_attributes'] )) && ($item->getProductType() == 'simple' && $item->getData('price') == 0 || $item->getData('parent_item_id') != '') ){
+                    $discountAmount = $bundProduct[$item->getData('parent_item_id')]['discountAmount'];
+                    $priceBundle = $bundProduct[$item->getData('parent_item_id')]['priceBundle2'] * $bundProduct[$item->getData('parent_item_id')]['totalQtyBundle'];
+                    //$totalQty = $bundProduct[$item->getData('parent_item_id')]['totalQty'];
+                    //$totalQtyBundle = $bundProduct[$item->getData('parent_item_id')]['totalQtyBundle'];
+
+                    $bundleDiscount = (($discountAmount / $priceBundle) * $finalPriceItem );
+
+                }
+                if( isset($confProductDiscount[$item->getData('parent_item_id')]) ){
+                    $disc = $confProductDiscount[$item->getData('parent_item_id')];
+                    $discountPerItem = - (float) (round(($disc / $item->getData('qty_ordered')),3));
+                }else{
+                    $discountPerItem = - (float) ($bundleDiscount + round(($item->getData('discount_amount') / $item->getData('qty_ordered')),3));
+                }
+                /**/
                 foreach ($customFieldsConfig as $customFieldsConfigItem) {
                     switch ($customFieldsConfigItem['netsuite_field_name']) {
                         case 'custcol_magentoitemid':
@@ -246,7 +265,7 @@ class RocketWeb_Netsuite_Helper_Mapper_Order extends RocketWeb_Netsuite_Helper_M
                             if (in_array($item->getProductType(), array ('bundle'))) {
                                 continue;
                             }
-                            $bundleDiscount = 0;
+                            /*$bundleDiscount = 0;
                             $discountPerItem = 0;
                             if( (isset( $productOptions['bundle_selection_attributes'] )) && ($item->getProductType() == 'simple' && $item->getData('price') == 0 || $item->getData('parent_item_id') != '') ){
                                 $discountAmount = $bundProduct[$item->getData('parent_item_id')]['discountAmount'];
@@ -262,7 +281,7 @@ class RocketWeb_Netsuite_Helper_Mapper_Order extends RocketWeb_Netsuite_Helper_M
                                 $discountPerItem = - (float) (round(($disc / $item->getData('qty_ordered')),3));
                             }else{
                                 $discountPerItem = - (float) ($bundleDiscount + round(($item->getData('discount_amount') / $item->getData('qty_ordered')),3));
-                            }
+                            }*/
 
                             $totalDiscount += $discountPerItem;
                             $item->setData('discount_amount', $discountPerItem);
@@ -276,21 +295,22 @@ class RocketWeb_Netsuite_Helper_Mapper_Order extends RocketWeb_Netsuite_Helper_M
                             
                             $bilna_credit = $pointsTransaction->getData('base_points_to_money');
                             $subTotal = $magentoOrder->getSubtotal();
+                            $orderDiscountAmount = $magentoOrder->getDiscountAmount();
                             $bilnaCreditItem = 0;
                             if ($item->getProductType() == 'simple' && $item->getData('price') == 0 || $item->getData('parent_item_id') != '') {
                                 if (isset ($productOptions['bundle_selection_attributes'])) {
                                     //$bilna_credit = $pointsTransaction->getData('base_points_to_money');
                                     //$subTotal = $magentoOrder->getSubtotal();
-                                    $bilnaCreditItem = $finalPriceItem * ($bilna_credit / $subTotal);
+                                    $bilnaCreditItem = ( $finalPriceItem - $discountPerItem ) * ($bilna_credit / ($subTotal - $orderDiscountAmount) );
                                 }
                                 
                                 if (!empty ($confProduct) && $confProduct[$item->getData('parent_item_id')] > 0) {
-                                    $bilnaCreditItem = $confProduct[$item->getData('parent_item_id')] * ($bilna_credit / $subTotal);
+                                    $bilnaCreditItem = ($confProduct[$item->getData('parent_item_id')] - $discountPerItem ) * ($bilna_credit / ($subTotal - $orderDiscountAmount) );
                                 }
                             }else{
                                 //$bilna_credit = $pointsTransaction->getData('base_points_to_money');
                                 //$subTotal = $magentoOrder->getSubtotal();
-                                $bilnaCreditItem = $item->getData('price') * ($bilna_credit / $subTotal);
+                                $bilnaCreditItem = ($item->getData('price') - $discountPerItem) * ($bilna_credit / ($subTotal - $orderDiscountAmount) );
                             }
                             
                             $pointsTransaction->setData('base_points_to_money', $bilnaCreditItem);
