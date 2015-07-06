328f9ffecee76a27dc61b0ce3068dff42b3558b5
Dhany Noor Alfian <dhany@bilna.com>
2015-07-06 14:22:44 +0700

    API Product & Category - add new API Category - fix API Product Collection

---
+++ app/code/local/Mage/Catalog/Model/Api2/Category/Rest.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Description of Mage_Catalog_Model_Api2_Category_Rest
+ *
+ * @author Bilna Development Team <development@bilna.com>
+ */
+
+abstract class Mage_Catalog_Model_Api2_Category_Rest extends Mage_Catalog_Model_Api2_Product {
+    
+}
