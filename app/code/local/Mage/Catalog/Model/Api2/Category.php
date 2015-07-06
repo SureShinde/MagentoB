a3ae76a6ed2910d10a62683bae302e8e042308bf
Dhany Noor Alfian <dhany@bilna.com>
2015-07-06 17:06:13 +0700

    API Product & Category - fix category detail

--- app/code/local/Mage/Catalog/Model/Api2/Category.php
+++ app/code/local/Mage/Catalog/Model/Api2/Category.php
@@ -6,9 +6,5 @@
  */
 
 class Mage_Catalog_Model_Api2_Category extends Mage_Api2_Model_Resource {
-    const DEFAULT_STORE_ID = 1;
     
-    protected function _getStore() {
-        return Mage::getModel('core/store')->load(self::DEFAULT_STORE_ID);
-    }
 }
