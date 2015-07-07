664bc4cb2fae175a76b3e79e2471c9bb19a8f5e1
Dhany Noor Alfian <dhany@bilna.com>
2015-07-07 08:48:05 +0700

    API Product & Category - add new endpoint retrieve megamenu - fix endpoint retrieve categories

---
+++ app/code/local/Bilna/Rest/Model/Api2/Category/Rest.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Description of Bilna_Rest_Model_Api2_Category_Rest
+ *
+ * @author Bilna Development Team <development@bilna.com>
+ */
+
+abstract class Bilna_Rest_Model_Api2_Category_Rest extends Bilna_Rest_Model_Api2_Category {
+    
+}
