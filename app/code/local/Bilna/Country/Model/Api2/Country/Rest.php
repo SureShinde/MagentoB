b922abe7528c0655ed084396e207af53acb31cae
Richi Prathama <richie@bilna.com>
2015-07-09 14:32:59 +0700

    fix newsletter & add countries endpoint

---
+++ app/code/local/Bilna/Country/Model/Api2/Country/Rest.php
@@ -0,0 +1,49 @@
+<?php
+
+abstract class Bilna_Country_Model_Api2_Country_Rest extends Bilna_Country_Model_Api2_Country
+{
+
+    /**
+     * Get customers list
+     *
+     * @return array
+     */
+    protected function _retrieveCollection()
+    {
+        $data = $this->_getCollectionForRetrieve()
+            ->load()
+            ->toArray();
+        
+        return isset($data['items']) ? $data['items'] : $data;
+    }
+
+    /**
+     * Retrieve collection instances
+     *
+     * @return Mage_Customer_Model_Resource_Customer_Collection
+     */
+    protected function _getCollectionForRetrieve()
+    {
+        /**
+         *
+         * @var $collection Mage_Customer_Model_Resource_Customer_Collection
+         */
+        $collection = Mage::getModel('premiumshipping/premiumshipping')->getCollection();
+        // $collection = Mage::getResourceModel('directory/region_collection');
+        $collection->getSelect()
+            ->join(array(
+            "a" => "directory_country_region"
+        ), "a.region_id = main_table.dest_region_id", array(
+            "country_id",
+            "region_id",
+            "code",
+            "default_name"
+        ))
+            ->group('main_table.dest_city');
+        
+        // $collection->addAttributeToSelect(array_keys($this->getAvailableAttributes($this->getUserType(), Mage_Api2_Model_Resource::OPERATION_ATTRIBUTE_READ)));
+        $this->_applyCollectionModifiers($collection);
+        
+        return $collection;
+    }
+}
