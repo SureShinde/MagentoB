edd9ba35ee8ed22e41d4dc00a5683922c7dc0c17
Dhany Noor Alfian <dhany@bilna.com>
2015-07-13 14:52:36 +0700

    API Product Reviews & Ratings - add response Rating

--- app/code/local/Bilna/Rest/Model/Api2/Product/Review/Rest/Admin/V1.php
+++ app/code/local/Bilna/Rest/Model/Api2/Product/Review/Rest/Admin/V1.php
@@ -11,7 +11,8 @@ class Bilna_Rest_Model_Api2_Product_Review_Rest_Admin_V1 extends Bilna_Rest_Mode
             ->addStoreFilter($this->_getStore()->getId())
             ->addEntityFilter('product', $this->_getProduct()->getId())
             ->addStatusFilter(Mage_Review_Model_Review::STATUS_APPROVED)
-            ->setDateOrder();
+            ->setDateOrder()
+            ->addRateVotes();
         
         $this->_applyCollectionModifiers($collection);
         $reviews = $collection->load();
@@ -28,11 +29,9 @@ class Bilna_Rest_Model_Api2_Product_Review_Rest_Admin_V1 extends Bilna_Rest_Mode
             $ratingVotes = $review->getRatingVotes();
             $votes = array ();
             
-            if (count($ratingVotes) > 0) {
-                foreach ($ratingVotes as $ratingVote) {
-                    $votes['rating_code'] = $ratingVote->getRatingCode();
-                    $votes['percent'] = $ratingVote->getPercent();
-                }
+            foreach ($ratingVotes as $ratingVote) {
+                $votes['rating_code'] = $ratingVote->getRatingCode();
+                $votes['percent'] = $ratingVote->getPercent();
             }
             
             $result[$i] = array (
