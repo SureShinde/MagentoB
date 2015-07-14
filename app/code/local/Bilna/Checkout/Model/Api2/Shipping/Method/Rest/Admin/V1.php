47df6241ad74983163968d49cb2843169efbc2d4
Taufik Ramdani <taufik.r@bilna.com>
2015-07-14 17:53:20 +0700

    get shipping method list

---
+++ app/code/local/Bilna/Checkout/Model/Api2/Shipping/Method/Rest/Admin/V1.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * API2 class for shipping method(admin)
+ *
+ * @category   Bilna
+ * @package    Bilna_Checkout
+ * @author     Development Team <development@bilna.com>
+ */
+class Bilna_Checkout_Model_Api2_Shipping_Method_Rest_Admin_V1 extends Bilna_Checkout_Model_Api2_Shipping_Method_Rest
+{
+	/**
+     * Set an Shipping Method for Shopping Cart
+     *
+     * @param  $quoteId
+     * @param  $shippingMethod
+     * @param  $store
+     * @return bool
+     */
+    protected function _retrieve()
+    {
+    	die("hjhdsbcjdsbf");
+    }
+
+}
