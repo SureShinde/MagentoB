190174d18ef851f5dd453c2b5fa15e7da015b82b
Richi Prathama <richie@bilna.com>
2015-07-24 13:29:21 +0700

    sent email at register and forgot password

---
+++ app/code/local/Bilna/Customer/Model/Api2/Resetpassword/Rest.php
@@ -0,0 +1,24 @@
+<?php
+
+abstract class Bilna_Customer_Model_Api2_Resetpassword_Rest extends Bilna_Customer_Model_Api2_Resetpassword
+{
+
+    protected function _update(array $data)
+    {
+        $customer = Mage::getModel('customer/customer')
+            ->setWebsiteId(1)
+            ->loadByEmail($data["email"]);
+
+        if ($customer->getId()) {
+            try {
+                $newResetPasswordLinkToken =  Mage::helper('customer')->generateResetPasswordLinkToken();
+                $customer->changeResetPasswordLinkToken($newResetPasswordLinkToken);
+                $customer->sendPasswordResetConfirmationEmail();
+            } catch (Exception $exception) {
+                $this->_error($exception->getMessage(), Mage_Api2_Model_Server::HTTP_INTERNAL_ERROR);
+            }
+        }else{
+            $this->_error("No email exist!", Mage_Api2_Model_Server::HTTP_INTERNAL_ERROR);
+        }
+    }
+}
