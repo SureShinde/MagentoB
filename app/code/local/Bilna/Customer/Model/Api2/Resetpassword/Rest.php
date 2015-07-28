49fe57a84197d0b514622f1fcdf022aff67fe6d5
Richi Prathama <richie@bilna.com>
2015-07-28 13:04:12 +0700

    add region_id and fix forgot password

--- app/code/local/Bilna/Customer/Model/Api2/Resetpassword/Rest.php
+++ app/code/local/Bilna/Customer/Model/Api2/Resetpassword/Rest.php
@@ -7,6 +7,44 @@ abstract class Bilna_Customer_Model_Api2_Resetpassword_Rest extends Bilna_Custom
     {
         $customer = Mage::getModel('customer/customer')
             ->setWebsiteId(1)
+            ->loadByEmail($this->getRequest()->getParam('email'));
+
+        if ($customer->getId()) {
+            try {
+                $newResetPasswordLinkToken =  Mage::helper('customer')->generateResetPasswordLinkToken();
+                $customer->changeResetPasswordLinkToken($newResetPasswordLinkToken);
+                $customer->sendPasswordResetConfirmationEmail();
+            } catch (Exception $exception) {
+                $this->_error($exception->getMessage(), Mage_Api2_Model_Server::HTTP_INTERNAL_ERROR);
+            }
+        }
+        
+        return true;
+    }
+
+    protected function _retrieve()
+    {
+        $customer = Mage::getModel('customer/customer')
+            ->setWebsiteId(1)
+            ->loadByEmail($this->getRequest()->getParam('email'));
+
+        if ($customer->getId()) {
+            try {
+                $newResetPasswordLinkToken =  Mage::helper('customer')->generateResetPasswordLinkToken();
+                $customer->changeResetPasswordLinkToken($newResetPasswordLinkToken);
+                $customer->sendPasswordResetConfirmationEmail();
+            } catch (Exception $exception) {
+                $this->_error($exception->getMessage(), Mage_Api2_Model_Server::HTTP_INTERNAL_ERROR);
+            }
+        }
+        
+        return true;
+    }
+
+    protected function _update(array $data)
+    {
+        $customer = Mage::getModel('customer/customer')
+            ->setWebsiteId(1)
             ->loadByEmail($data["email"]);
 
         if ($customer->getId()) {
