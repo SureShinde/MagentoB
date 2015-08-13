bb0b3c021cc61996fe5b132170a74c4bbb6af4e7
Taufik Ramdani <taufikramdani07@gmail.com>
2015-08-13 19:58:05 +0700

    add bank check validation for cc payment method

--- app/code/local/Bilna/Checkout/Model/Api2/Payment/Method/Bankcheck/Rest/Admin/V1.php
+++ app/code/local/Bilna/Checkout/Model/Api2/Payment/Method/Bankcheck/Rest/Admin/V1.php
@@ -50,6 +50,49 @@ class Bilna_Checkout_Model_Api2_Payment_Method_Bankcheck_Rest_Admin_V1 extends B
         return $response;
     }
 
+    /**
+     * Get Bank Check Validation
+     *
+     * @param  $quoteId
+     * @param  $shippingMethod
+     * @param  $store
+     * @return bool
+     */
+    protected function _retrieve()
+    {
+        $cardNo = $this->getRequest()->getParam('id');
+        $response = array (
+            'status' => false,
+            'data' => array (),
+            'message' => null
+        );
+
+        try {
+            if (in_array ($cardNo[0], array (4,5)))
+            {
+                $bankCode = Mage::getModel('paymethod/method_vtdirect')->getBankCode($cardNo);
+                $ccType = $this->getCcType($bankCode);
+                
+                $response['status'] = true;
+                $response['data'] = array (
+                    'bank_code' => $bankCode,
+                    'cc_type' => $ccType,
+                    'acquired_bank' => $this->getAcquiredBank($bankCode),
+                    'secure' => $this->getSecureBank($bankCode),
+                    'installment_process' => $this->getInstallmentProcess($bankCode)
+                );
+            }
+            else {
+                $response['message'] = 'Please enter a valid credit card number.';
+            }
+
+        } catch (Mage_Core_Exception $e) {
+            $this->_error($e->getMessage(), Mage_Api2_Model_Server::HTTP_INTERNAL_ERROR);
+        }
+
+        return $response;
+    }
+
     protected function getCcType($bank)
     {
         $ccType = (strtoupper(substr($bank, -2)) == 'MC') ? 'MC' : 'VI';
