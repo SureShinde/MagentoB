51e4d61255aa25705138b63aec49a0a0f3446e64
Dhany Noor Alfian <dhany@bilna.com>
2015-08-21 16:21:51 +0700

    API Product Related & Featured - fix response Product Related - fix response Proudct Featured

--- app/code/local/Bilna/Rest/Model/Api2.php
+++ app/code/local/Bilna/Rest/Model/Api2.php
@@ -8,7 +8,23 @@
 class Bilna_Rest_Model_Api2 extends Mage_Api2_Model_Resource {
     const DEFAULT_STORE_ID = 1;
     
+    protected $_data = array ();
+    
     protected function _getStore() {
         return Mage::getModel('core/store')->load(self::DEFAULT_STORE_ID);
+    }
+    
+    protected function _createValidator($validatorModel) {
+        $validator = Mage::getModel($validatorModel);
+        
+        if ($this->_data) {
+            if (!$validator->isValidData($this->_data)) {
+                foreach ($validator->getErrors() as $error) {
+                    $this->_error($error, Mage_Api2_Model_Server::HTTP_BAD_REQUEST);
+                }
+
+                $this->_critical(self::RESOURCE_DATA_PRE_VALIDATION_ERROR);
+            }
+        }
     }
 }
--- app/code/local/Bilna/Rest/Model/Api2/Productfeatured/Rest.php
+++ app/code/local/Bilna/Rest/Model/Api2/Productfeatured/Rest.php
@@ -6,5 +6,45 @@
  */
 
 abstract class Bilna_Rest_Model_Api2_Productfeatured_Rest extends Bilna_Rest_Model_Api2_Productfeatured {
+    protected $_blockId = null;
+    protected $_block = null;
+
+    protected function _getParams() {
+        $this->_blockId = $this->getRequest()->getParam('block_id');
+    }
     
+    protected function _getBlock() {
+        if (is_null($this->_block)) {
+            $this->_block = Mage::getModel('awfeatured/blocks')->setStoreId($this->_getStore()->getId())->loadByBlockId($this->_blockId);
+            
+            if (!$this->_block->getId()) {
+                $this->_error('Block not found', Mage_Api2_Model_Server::HTTP_NOT_FOUND);
+                $this->_critical(self::RESOURCE_NOT_FOUND);
+            }
+        }
+        
+        return $this->_block;
+    }
+    
+    protected function _parseBlock($blockData) {
+        if (isset ($blockData['automation_data']) && isset ($blockData['automation_data']['products'])) {
+            $limit = isset ($blockData['type_data']['productsinrow']) ? $blockData['type_data']['productsinrow'] : 10;
+            $products = explode(",", $blockData['automation_data']['products']);
+            $productsArr = array ();
+            $x = 0;
+            
+            foreach ($products as $product) {
+                if ($x == $limit) {
+                    break;
+                }
+                
+                $productsArr[] = $product;
+                $x++;
+            }
+            
+            $blockData['automation_data']['products'] = $productsArr;
+        }
+        
+        return $blockData;
+    }
 }
