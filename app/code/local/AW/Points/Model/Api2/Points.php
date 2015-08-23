ef19245732e1965a397b488a4fc5aba8ba49c7f8
Taufik Ramdani <taufikramdani07@gmail.com>
2015-08-23 19:32:02 +0700

    add retrieve api points avability for customer registered

--- app/code/local/AW/Points/Model/Api2/Points.php
+++ app/code/local/AW/Points/Model/Api2/Points.php
@@ -9,5 +9,17 @@
  */
 class AW_Points_Model_Api2_Points extends Mage_Api2_Model_Resource
 {
-
+    /**
+     *
+     */
+    protected function _getCustomer($customerId)
+    {
+        /** @var $customer Mage_Customer_Model_Customer */
+        $customer = Mage::getModel('customer/customer')
+            ->load($customerId);
+        if (!$customer->getId()) {
+            throw Mage::throwException('Customer Not Exists');
+        }
+        return $customer;
+    }
 }
