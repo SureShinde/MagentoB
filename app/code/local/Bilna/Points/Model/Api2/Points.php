1bbd0bb4dcee88b76a0d46116589726a4de0cccc
Taufik Ramdani <taufik.r@bilna.com>
2015-08-25 12:55:34 +0700

    finalize bilna credit availbility

--- app/code/local/Bilna/Points/Model/Api2/Points.php
+++ app/code/local/Bilna/Points/Model/Api2/Points.php
@@ -22,4 +22,29 @@ class Bilna_Points_Model_Api2_Points extends Mage_Api2_Model_Resource
         }
         return $customer;
     }
+
+    /**
+     * Retrieves quote by quote identifier and store code or by quote identifier
+     *
+     * @param int $quoteId
+     * @param string|int $store
+     * @return Mage_Sales_Model_Quote
+     */
+    protected function _getQuote($quoteId, $storeId = 1)
+    {
+        /** @var $quote Mage_Sales_Model_Quote */
+        $quote = Mage::getModel("sales/quote");
+
+        if (!(is_string($storeId) || is_integer($storeId))) {
+            $quote->loadByIdWithoutStore($quoteId);
+        } else {
+            $quote->setStoreId($storeId)
+                    ->load($quoteId);
+        }
+        if (is_null($quote->getId())) {
+            Mage::throwException("Quote Not Exists");
+        }
+
+        return $quote;
+    }
 }
