16bd6c31da9569ed9d283eeb014fd4e71559225c
harfil <harfil@bilna.com>
2015-08-26 11:52:37 +0700

    fixing wrong commit in wrong branch

--- app/code/local/Bilna/Pricevalidation/Model/Form.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class Bilna_Pricevalidation_Model_Form extends Mage_Core_Model_Abstract
-{
-    public function _construct()
-    {
-        $this->_init('bilna_pricevalidation/form');
-    }
-}
