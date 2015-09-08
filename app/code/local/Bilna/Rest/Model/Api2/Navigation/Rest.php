43b7e9c7ab61054cdb4fe2976c237cfb39606eaa
Dhany Noor Alfian <dhany@bilna.com>
2015-09-08 18:11:10 +0700

    API Layered Navigation - add new endpoint for Category Page

--- app/code/local/Bilna/Rest/Model/Api2/Navigation/Rest.php
+++ app/code/local/Bilna/Rest/Model/Api2/Navigation/Rest.php
@@ -6,5 +6,64 @@
  */
 
 abstract class Bilna_Rest_Model_Api2_Navigation_Rest extends Bilna_Rest_Model_Api2_Navigation {
+    protected $_categoryId = null;
     
+    protected function _getParams() {
+        $_params = array ();
+        $_params['category_id'] = $this->getRequest()->getParam('category_id');
+        
+        return $_params;
+    }
+    
+    protected function _getCategory($_categoryId) {
+        return Mage::getModel('catalog/category')->load($_categoryId);
+    }
+
+    protected function _getSubcategory($_category) {
+        $_categories = $_category->getChildrenCategories();
+        $_result = array ();
+        
+        foreach ($_categories as $_category) {
+            if ($_category->getIsActive() && $_category->getProductCount()) {
+                $_result[] = array (
+                    'label' => Mage::helper('core')->escapeHtml($_category->getName()),
+                    'value' => $_category->getId(),
+                    'count' => $_category->getProductCount(),
+                );
+            }
+        }
+        
+        return $_result;
+    }
+
+    protected function _getLayer($_category) {
+        $_layer = Mage::getModel('catalog/layer');
+        $_layer->setCurrentCategory($_category);
+        $_attributes = $_layer->getFilterableAttributes();
+        $_result = array ();
+        
+        foreach ($_attributes as $_attribute) {
+            if ($_attribute->getAttributeCode() == 'price') {
+                $_filterBlockName = 'catalog/layer_filter_price';
+            }
+            elseif ($_attribute->getBackendType() == 'decimal') {
+                $_filterBlockName = 'catalog/layer_filter_decimal';
+            }
+            else {
+                $_filterBlockName = 'catalog/layer_filter_attribute';
+            }
+            
+            $_options = Mage::app()->getLayout()->createBlock($_filterBlockName)->setLayer($_layer)->setAttributeModel($_attribute)->init();
+            
+            foreach ($_options->getItems() as $_option) {
+                $_result[$_attribute->getAttributeCode()][] = array (
+                    'label' => Mage::helper('core')->escapeHtml($_option->getLabel()),
+                    'value' => $_option->getValue(),
+                    'count' => $_option->getCount(),
+                );
+            }
+        }
+        
+        return $_result;
+    }
 }
--- app/code/local/Bilna/Rest/Model/Api2/Navigation/Rest/Admin/V1.php
+++ app/code/local/Bilna/Rest/Model/Api2/Navigation/Rest/Admin/V1.php
@@ -6,57 +6,15 @@
  */
 
 class Bilna_Rest_Model_Api2_Navigation_Rest_Admin_V1 extends Bilna_Rest_Model_Api2_Navigation_Rest {
-    protected function _retrieveCollection() {
-        echo "masup sini dulu";exit;
-    }
-    
-    protected function _canShowBlock() {
-        if ($this->_canShowOptions()) {
-            return true;
-        }
+    protected function _retrieve() {
+        $_params = $this->_getParams();
+        $_categoryId = $_params['category_id'];
+        $_category = $this->_getCategory($_categoryId);
         
-        $cnt = 0;
-        $pos = Mage::getStoreConfig('amshopby/block/state_pos'); 
+        $_result = array ();
+        $_result['subcategory'] = $this->_getSubcategory($_category);
+        $_result['layer'] = $this->_getLayer($_category);
         
-        if (!$this->_notInBlock($pos)) {
-            $cnt = count($this->getLayer()->getState()->getFilters());
-        }
-        
-        return $cnt;
-    }
-    
-    /**
-     * Check availability display layer options
-     *
-     * @return bool
-     */
-    protected function _canShowOptions() {
-        foreach ($this->getFilters() as $filter) {
-            if ($filter->getItemsCount()) {
-                return true;
-            }
-        }
-
-        return false;
-    }
-    
-    /**
-     * Get all layer filters
-     *
-     * @return array
-     */
-    public function getFilters() {
-        $filters = array ();
-        
-        if ($categoryFilter = $this->_getCategoryFilter()) {
-            $filters[] = $categoryFilter;
-        }
-
-        $filterableAttributes = $this->_getFilterableAttributes();
-        foreach ($filterableAttributes as $attribute) {
-            $filters[] = $this->getChild($attribute->getAttributeCode() . '_filter');
-        }
-
-        return $filters;
+        return $_result;
     }
 }
