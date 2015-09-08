d87947edfabcdc1d126a7684b1c02842b66ef38a
Taufik Ramdani <taufikramdani07@gmail.com>
2015-09-08 17:32:57 +0700

    add endpoint API for checking veritrans charging response

---
+++ app/code/local/Bilna/Paymethod/Model/Api2/Vtdirect/Rest.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * API2 class for rest (admin)
+ *
+ * @category   Bilna
+ * @package    Bilna_Paymethod
+ * @author     Development Team <development@bilna.com>
+ */
+abstract class Bilna_Paymethod_Model_Api2_Vtdirect_Rest extends Bilna_Paymethod_Model_Api2_Vtdirect
+{
+
+}
