4669e8534fad374690addbd354ac3a3371c6477e
Dhany Noor Alfian <dhany@bilna.com>
2015-09-23 14:47:39 +0700

    API Category - add new response thumbnail and image

--- app/code/local/Bilna/Rest/Model/Api2/Category/Rest.php
+++ app/code/local/Bilna/Rest/Model/Api2/Category/Rest.php
@@ -6,5 +6,7 @@
  */
 
 abstract class Bilna_Rest_Model_Api2_Category_Rest extends Bilna_Rest_Model_Api2_Category {
-    
+    protected function _getCategoryImageUrl($_image) {
+        return sprintf("%scatalog/category/%s", Mage::getBaseUrl('media'), $_image);
+    }
 }
