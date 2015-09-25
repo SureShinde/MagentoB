7b8f7a2cb4ca2ff14f29a41b6ccbd9ca028aa503
Dhany Noor Alfian <dhany@bilna.com>
2015-09-25 13:46:50 +0700

    BILNA #New Payment Method Mandiri E-Cash - fix configuration

--- app/code/local/Bilna/Paymethod/Model/Method/Mandiriecash.php
+++ app/code/local/Bilna/Paymethod/Model/Method/Mandiriecash.php
@@ -6,7 +6,7 @@
  */
 
 class Bilna_Paymethod_Model_Method_Mandiriecash extends Bilna_Paymethod_Model_Method_Vtdirect {
-    protected $_code = 'mandiri_ecash';
+    protected $_code = 'mandiriecash';
     protected $_formBlockType = 'paymethod/form_mandiriecash';
     protected $_infoBlockType = 'paymethod/info_mandiriecash';
 }
