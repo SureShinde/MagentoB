268ef0899138c0577712e0c10ddccb8420b94625
I Made Raka Teja <raka.teja@bilna.com>
2015-09-30 12:00:50 +0700

    improve formbuilder feature

--- app/code/local/Bilna/Formbuilder/Block/Adminhtml/Formbuilder/Edit/Tabs/Edit/Tabs/Detail.php
+++ app/code/local/Bilna/Formbuilder/Block/Adminhtml/Formbuilder/Edit/Tabs/Edit/Tabs/Detail.php
@@ -2,54 +2,60 @@
 
 class Bilna_Formbuilder_Block_Adminhtml_Formbuilder_Edit_Tabs_Edit_Tabs_Detail extends Mage_Adminhtml_Block_Widget_Form
 {
-   protected function _prepareForm()
-   {
-		$formbuilder	= Mage::registry('formbuilder_form');
-    $form 				= new Varien_Data_Form();
-    $form->setUseContainer(true);
-		$this->setForm($form);
+	protected $inputId;
+
+   	protected function _prepareForm()
+   	{
+		//$formbuilder	= Mage::registry('formbuilder_form');
+		$this->inputId = (int) $this->getRequest()->getParam('id');
+	    //$form = new Varien_Data_Form();
+	    $form = new Varien_Data_Form(array('id' => 'edit_input',
+			'action' => $this->getUrl('*/*/saveInput', ['id' => $this->inputId]),
+			'method' => 'post', 
+			'enctype' => 'multipart/form-data'));
 		$form->setHtmlIdPrefix('formbuilder_');
 		$fieldset = $form->addFieldset('formbuilder_form', array('legend' => Mage::helper('bilna_formbuilder')->__('Inputs Detail')));
-
-		$collection = Mage::getModel('bilna_formbuilder/input')->getCollection();
-		$collection->addFieldToSelect('name');
-		$collection->addFieldToSelect('group');
-		$collection->addFieldToSelect('title');
-		$collection->addFieldToSelect('type');
-		//$collection->addFieldToSelect(new Zend_Db_Expr("if(required = 0, 'Enabled', 'Disabled')"), 'required');
-		$collection->addFieldToSelect('required');
-		//$collection->addFieldToSelect(new Zend_Db_Expr("if(`unique` = 0, 'Enabled', 'Disabled')"), 'unique');
-		$collection->addFieldToSelect('unique');
-		$collection->addFieldToSelect('order');
-		$collection->addFieldToFilter('main_table.id', (int) $this->getRequest()->getParam('id'));
-
-		$this->setCollection($collection);
-		$data = $collection->getFirstItem();
+		
+		$data = Mage::getModel('bilna_formbuilder/input')->findByParent($this->inputId);
 
 		//$fieldset->addField('name', 'label', array(
 		$fieldset->addField('name', 'text', array(
 		   'label'     => 'Name',
 		   'name'      => 'name',
 		   'value'     => $data["name"],
-    ));
+    	));
 
 		$fieldset->addField('group', 'text', array(
 		   'label'     => 'Group',
 		   'name'      => 'group',
 		   'value'     => $data["group"],
-    ));
+    	));
 
 		$fieldset->addField('title', 'text', array(
 		   'label'     => 'Title',
 		   'name'      => 'title',
 		   'value'     => $data["title"],
-    ));
+    	));
 
-		$fieldset->addField('type', 'text', array(
-		   'label'     => 'Type',
-		   'name'      => 'type',
-		   'value'     => $data["type"],
-    ));
+		$fieldset->addField('type', 'select', array(
+			'label'		=> 'Type',
+			'name'		=> 'type',
+			'value'		=> $data["type"],
+			'values'	=> $this->fieldTypeOptions()
+		));
+
+		$fieldset->addField('dbtype', 'select', array(
+			'label'	=> 'DbType',
+			'name'	=> 'dbtype',
+			'value' => $data['dbtype'],
+			'values' => $this->fieldDbTypeOptions()
+		));
+
+		$fieldset->addField('value', 'textarea', array(
+			'label' => 'Value',
+			'name' => 'value',
+			'value' => $data['value']
+		));
 
 		$fieldset->addField('required', 'select', array(
 		   'label'     => 'Required',
@@ -58,11 +64,11 @@ class Bilna_Formbuilder_Block_Adminhtml_Formbuilder_Edit_Tabs_Edit_Tabs_Detail e
        //'note'    => Mage::helper('bilna_formbuilder')->__('Status Note:</br>0=Enabled</br>1=Disabled'),
 		   'values'    => array(
              array(
-                 'value' => 0,
+                 'value' => 1,
                  'label' => Mage::helper('bilna_formbuilder')->__('Yes'),
              ),
              array(
-                 'value' => 1,
+                 'value' => 0,
                  'label' => Mage::helper('bilna_formbuilder')->__('No'),
              ),
          ),
@@ -75,22 +81,76 @@ class Bilna_Formbuilder_Block_Adminhtml_Formbuilder_Edit_Tabs_Edit_Tabs_Detail e
        //'note'    => Mage::helper('bilna_formbuilder')->__('Status Note:</br>0=Enabled</br>1=Disabled'),
 		   'values'    => array(
              array(
-                 'value' => 0,
+                 'value' => 1,
                  'label' => Mage::helper('bilna_formbuilder')->__('Yes'),
              ),
              array(
-                 'value' => 1,
+                 'value' => 0,
                  'label' => Mage::helper('bilna_formbuilder')->__('No'),
              ),
          ),
     ));
 
+		$fieldset->addField('helper_message', 'text', array(
+    		'label' => 'Helper message',
+    		'name' => 'helper_message',
+    		'value' => $data['helper_message']
+    	));
+
+    	$fieldset->addField('validation', 'text', array(
+    		'label' => 'Validation',
+    		'name' => 'validation',
+    		'value' => $data['validation']
+    	));
+
 		$fieldset->addField('order', 'text', array(
 		   'label'     => 'Order',
 		   'name'      => 'order',
 		   'value'     => $data["order"],
-    ));
+    	));
+
 		$form->setValues($data);
+		$form->setUseContainer(true);
+		$this->setForm($form);
     return parent::_prepareForm();
+	}
+
+	private function fieldTypeOptions()
+	{
+		return $this->renderOptions([
+			"text",
+			"textarea",
+			"radio",
+			"checkbox",
+			"dropdown",
+			"multiple",
+			"hidden",
+			"date",
+			"datetime"
+		]);
+	}
+
+	private function fieldDbTypeOptions()
+	{
+		return $this->renderOptions([
+			"int",
+			"varchar",
+			"date",
+			"datetime"
+		]);
+	}
+
+	private function renderOptions(array $data)
+	{
+		$options = [];
+		$helper = Mage::helper('bilna_formbuilder');
+		foreach ($data as $type) {
+			$options[] = array(
+					'value' => $type,
+					'label' => $helper->__(ucwords($type))
+				);
+		}
+
+		return $options;
 	}
 }
