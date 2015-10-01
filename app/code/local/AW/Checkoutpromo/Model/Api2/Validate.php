386a65ed9467753ff5e84008c368db6d257c6a52
Taufik Ramdani <taufik.r@bilna.com>
2015-10-01 18:08:45 +0700

    finalize api checkout promo

--- app/code/local/AW/Checkoutpromo/Model/Api2/Validate.php
+++ app/code/local/AW/Checkoutpromo/Model/Api2/Validate.php
@@ -9,7 +9,7 @@
  */
 class AW_Checkoutpromo_Model_Api2_Validate extends Mage_Api2_Model_Resource
 {
-	/**
+    /**
      * Retrieves quote by quote identifier and store code or by quote identifier
      *
      * @param int $quoteId
@@ -39,11 +39,14 @@ class AW_Checkoutpromo_Model_Api2_Validate extends Mage_Api2_Model_Resource
      */
     protected function _getCustomer($customerId)
     {
-                /** @var $customer Mage_Customer_Model_Customer */
+        /** @var $customer Mage_Customer_Model_Customer */
         $customer = Mage::getModel('customer/customer')
             ->load($customerId);
         if (!$customer->getId()) {
-            throw Mage::throwException('Customer Not Exists');
+            $customer = Mage::getModel('customer/customer');
+            $customer->setWebsiteId(1)
+                ->setCustomerGroupId(0);    
+            //throw Mage::throwException('Customer Not Exists');
         }
 
         return $customer;
--- app/code/local/AW/Checkoutpromo/Model/Api2/Validate/Rest/Admin/V1.php
+++ app/code/local/AW/Checkoutpromo/Model/Api2/Validate/Rest/Admin/V1.php
@@ -9,18 +9,18 @@
  */
 class AW_Checkoutpromo_Model_Api2_Validate_Rest_Admin_V1 extends AW_Checkoutpromo_Model_Api2_Validate_Rest
 {
-	protected function _retrieve()
+    protected function _retrieve()
     {
-    	$quoteId = $this->getRequest()->getParam('id');
-    	$customerId = $this->getRequest()->getParam('customer_id');
-    	$storeId = 1;
+        $quoteId = $this->getRequest()->getParam('id');
+        $customerId = $this->getRequest()->getParam('customer_id');
+        $storeId = 1;
 
-    	try{
-    		$quote = $this->_getQuote($quoteId, $storeId);
-    		$customer = $this->_getCustomer($customerId);
-    		$customerGroupId = $customer->getCustomerGroupId();
+        try{
+            $quote = $this->_getQuote($quoteId, $storeId);
+            $customer = $this->_getCustomer($customerId);
+            $customerGroupId = $customer->getCustomerGroupId();
 
-    		$validator = Mage::getModel('checkoutpromo/validator')
+            $validator = Mage::getModel('checkoutpromo/validator')
                     ->init($customer->getWebsiteId(), $customerGroupId);
 
             if (count($quote->getAllItems())) {
@@ -28,7 +28,7 @@ class AW_Checkoutpromo_Model_Api2_Validate_Rest_Admin_V1 extends AW_Checkoutprom
                 $_appliedBlockIds = $v->appliedBlockIds;
             }
 
-    	} catch (Mage_Core_Exception $e) {
+        } catch (Mage_Core_Exception $e) {
             $this->_error($e->getMessage(), Mage_Api2_Model_Server::HTTP_INTERNAL_ERROR);
         }
         
