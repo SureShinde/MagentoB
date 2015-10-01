5f297bd39d031b5b2451ef7db41858ef69b3843c
Taufik Ramdani <taufik.r@bilna.com>
2015-10-01 18:09:10 +0700

    add store id to get system config magento

--- app/code/local/Bilna/System/Model/Api2/Config/Rest/Admin/V1.php
+++ app/code/local/Bilna/System/Model/Api2/Config/Rest/Admin/V1.php
@@ -9,14 +9,15 @@
  */
 class Bilna_System_Model_Api2_Config_Rest_Admin_V1 extends Bilna_System_Model_Api2_Config_Rest
 {
+    static $STORE_ID = 1;
 
-	protected function _retrieve()
+    protected function _retrieve()
     {
         try{
-        	$key = $this->getRequest()->getParam('id');
-        	$keyConfig = str_replace("-", "/", $key);
+            $key = $this->getRequest()->getParam('id');
+            $keyConfig = str_replace("-", "/", $key);
 
-        	$config = Mage::getStoreConfig($keyConfig);
+            $config = Mage::getStoreConfig($keyConfig, self::$STORE_ID);
 
         } catch (Mage_Core_Exception $e) {
                 $this->_error($e->getMessage(), Mage_Api2_Model_Server::HTTP_INTERNAL_ERROR);
