575fa013f30375be3668261a3b39321792662704
harfil <harfil@bilna.com>
2015-10-06 16:54:19 +0700

    ketinggalan fix

--- app/code/local/Bilna/Pricevalidation/Block/Adminhtml/Render/Logfile.php
+++ app/code/local/Bilna/Pricevalidation/Block/Adminhtml/Render/Logfile.php
@@ -2,10 +2,9 @@
     class Bilna_Pricevalidation_Block_Adminhtml_Render_Logfile extends Mage_Adminhtml_Block_Widget_Grid_Column_Renderer_Abstract
     {
         public function render(Varien_Object $row) {
-            $baseDir = '';
-            $model  = Mage::getModel('bilna_pricevalidation/profile')->load($row->getData('profile_id'));
-            if(!empty($model->getBaseDir())) {
-                $baseDir = $model->getBaseDir().'/';
+            $baseDir = $row->getData('base_dir');
+            if(!empty($baseDir)) {
+                $baseDir .= '/';
             }
             if($row->getData('rows_errors') != 0) {
                 $data = '<a href="'.Mage::getBaseUrl(Mage_Core_Model_Store::URL_TYPE_WEB).'files/pricevalidation/import/'.$baseDir.$row->getData('error_file').'">Download</a>';
