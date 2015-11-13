597b5cfe9630d5c1addf1c4bf13fd63a8407aacb
M. KHAIRUL AZAMI, S. KOM <khairul.azami@bilna.com>
2015-11-13 16:25:39 +0700

    add createreport for form fields API

--- app/code/local/AW/Affiliate/Model/Api2/Producttrackinglink.php
+++ app/code/local/AW/Affiliate/Model/Api2/Producttrackinglink.php
@@ -8,5 +8,11 @@
  * @author     Bilna Development Team <core@magentocommerce.com>
  */
 class AW_Affiliate_Model_Api2_Producttrackinglink extends Mage_Api2_Model_Resource
-{
+{    
+    const DEFAULT_STORE_ID = 1;
+
+    public function __construct() 
+    {
+        Mage::app()->getStore()->setStoreId(self::DEFAULT_STORE_ID);
+    }
 }
