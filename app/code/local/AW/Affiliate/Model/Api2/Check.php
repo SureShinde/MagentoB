074f589f48f88ec70f44c1fe0c8c46c1ba5d4aac
M. KHAIRUL AZAMI, S. KOM <khairul.azami@bilna.com>
2015-11-24 16:44:30 +0700

    adjustment code of affiliate check API and add affiliate order complete API

--- app/code/local/AW/Affiliate/Model/Api2/Check.php
+++ app/code/local/AW/Affiliate/Model/Api2/Check.php
@@ -9,175 +9,6 @@
  */
 class AW_Affiliate_Model_Api2_Check extends Mage_Api2_Model_Resource
 {
-    const MINIMUM_WITHDRAWAL_PERIOD_REGISTRY_KEY = '_awaff_mwrk';
-
-    public function customerLogin($observer)
-    {
-        $customerId = Mage::getSingleton('customer/session')->getId();
-
-        if (!$customerId) return;
-        $cookieModel = Mage::getSingleton('core/cookie');
-        $campaingCollection = Mage::getModel('awaffiliate/campaign')->getCollection();
-
-        foreach ($campaingCollection as $campaingItem) {
-            $clientId = $cookieModel->get(AW_Affiliate_Helper_Config::COOKIE_NAME);
-            if ($clientId) {
-                $clientItem = Mage::getModel('awaffiliate/client')->load($clientId);
-                if (($clientItem->getData()) && ($clientItem->getCustomerId() == NULL)) {
-                    $clientItem->setData('customer_id', $customerId);
-                    $clientItem->save();
-                }
-            }
-        }
-    }
-
-    public function orderPlaced($observer)
-    {
-        $order = $observer->getOrder();
-        $client = Mage::helper('awaffiliate')->getAffiliateCookie();
-
-        //add client history
-        if (!is_null($client)) {
-            $clientModel = Mage::getModel('awaffiliate/client')->load($client);
-            $affiliateModel = Mage::getModel('awaffiliate/affiliate')->load($clientModel->getAffiliateId());
-
-            if (is_null($clientModel->getId())) {
-                return;
-            }
-            if ($order->getCustomerId() == $affiliateModel->getCustomerId()) {
-                return;
-            }
-            $historyModel = Mage::getModel('awaffiliate/client_history');
-            $historyModel->setData(array(
-                'client_id' => $clientModel->getId(),
-                'action' => AW_Affiliate_Model_Source_Client_Actions::ORDER_PLACED,
-                'linked_item_id' => $order->getId(),
-                'created_at' => Mage::getModel('core/date')->gmtDate(),
-                'params' => array()
-            ));
-            try {
-                $historyModel->save();
-            } catch (Exception $e) {
-                //TODO: log
-            }
-        }
-    }
-
-    public function invoicePay($observer)
-    {
-        /** @var $invoice Mage_Sales_Model_Order_Invoice */
-        $invoice = $observer->getInvoice();
-        /** @var $order Mage_Sales_Model_Order */
-        $order = $invoice->getOrder();
-        /** @var $quote Mage_Sales_Model_Quote */
-        $quote = Mage::getModel('sales/quote');
-
-        if ($invoice->getOrigData() !== null) {
-            // Exit if invoice isn't new
-            return;
-        }
-
-        if (method_exists($quote, 'loadByIdWithoutStore')) {
-            $quote->loadByIdWithoutStore($order->getQuoteId());
-        } else {
-            $quote
-                ->setStoreId($order->getStoreId())
-                ->load($order->getQuoteId());
-        }
-        //Order placed type
-        $historyCollection = Mage::getModel('awaffiliate/client_history')->getCollection();
-        $historyCollection->onlyOrderPlacedAction();
-        $historyCollection->addLinkedItemIdFilter($invoice->getOrder()->getId());
-        if ($historyCollection->getSize() == 0) {
-            return;
-        }
-
-        foreach ($historyCollection as $action) {
-            $client = $action->getClient();
-
-            if (is_null($client->getId())) {
-                //TODO: log
-                continue;
-            }
-            if (!$this->_isAffiliateAccessible($client->getAffiliateId())) {
-                //TODO: log
-                continue;
-            }
-            if (!$this->_isCampaignAccessible($client->getCampaignId())) {
-                //TODO: log
-                continue;
-            }
-            if (!$this->_isAffiliateAllowedForCampaign($client->getAffiliateId(), $client->getCampaignId(), true)) {
-                //TODO: log
-                continue;
-            }
-
-            $campaign = $client->getCampaign();
-            $conditionsModel = $campaign->getConditionsModel();
-
-            $order->setQuote($quote);
-            if ($conditionsModel->getActions()->validate($order)) {
-                /** @var $trx AW_Affiliate_Model_Transaction_Profit */
-                $trx = Mage::getModel('awaffiliate/transaction_profit');
-                $trx->setData(array(
-                    'campaign_id' => $client->getData('campaign_id'),
-                    'affiliate_id' => $client->getData('affiliate_id'),
-                    'traffic_id' => $client->getData('traffic_id'),
-                    'client_id' => $client->getId(),
-                    'linked_entity_type' => AW_Affiliate_Model_Source_Transaction_Profit_Linked::INVOICE_ITEM,
-                    'linked_entity_id' => $order->getIncrementId(),
-                    'linked_entity_invoice' => $invoice,
-                    'linked_entity_order' => $order,
-                    'created_at' => Mage::getModel('core/date')->gmtDate(),
-                    'type' => AW_Affiliate_Model_Source_Transaction_Profit_Type::CUSTOMER_PURCHASE
-                ));
-                try {
-                    $trx->createTransaction();
-                } catch (Exception $e) {
-                    var_dump($e->getMessage());
-                    //TODO: log
-                }
-            }
-        }
-    }
-
-    public function customerSaveAfter($observer)
-    {
-        $customer = $observer->getCustomer();
-        $groupId = $customer->getGroupId();
-        $targetGroups = Mage::helper('awaffiliate/config')->getGroupsForAutoAffiliateCreating($customer->getStoreId());
-        $affiliate = Mage::getModel('awaffiliate/affiliate')->loadByCustomerId($customer->getId());
-        if (is_null($affiliate->getId()) && in_array($groupId, $targetGroups)) {
-            $affiliate->setData(array(
-                'customer_id' => $customer->getId(),
-                'status' => AW_Affiliate_Model_Source_Affiliate_Status::ACTIVE
-            ));
-            $affiliate->save();
-        }
-    }
-
-    public function withdrawalRequestSaveAfter($observer)
-    {
-        $request = $observer->getRequest();
-        $notify = Mage::helper('awaffiliate/notify');
-        if ($request->isStatusChangedToPaid()) {
-            $notify->sendNotifyAboutSuccessWithdrawalRequest($request);
-        }
-        if ($request->isStatusChangedToFailed() || $request->isStatusChangedToRejected()) {
-            $notify->sendNotifyAboutFailedWithdrawalRequest($request);
-        }
-        if ($request->isObjectNew()) {
-            $notify->sendNotifyAboutNewWithdrawalRequest($request);
-        }
-    }
-
-    public function _isCookieFree($cookieValue)
-    {
-        $client = Mage::getModel('awaffiliate/client')->load($cookieValue);
-        $__isRewriteCookieEnabled = Mage::helper('awaffiliate/config')->isRewriteCookieEnabled();
-        return is_null($client->getId()) || $__isRewriteCookieEnabled;
-    }
-
     public function _isAffiliateAccessible($affiliateId)
     {
         $_affiliate = Mage::getModel('awaffiliate/affiliate')->load($affiliateId);
@@ -193,7 +24,7 @@ class AW_Affiliate_Model_Api2_Check extends Mage_Api2_Model_Resource
     public function _isCampaignAccessible($campaignId)
     {
         $_campaign = Mage::getModel('awaffiliate/campaign')->load($campaignId);
-        return (!is_null($_campaign->getId()) && $_campaign->isActive());
+        return (!is_null($_campaign->getId())/* && $_campaign->isActive()*/);
     }
 
     public function _isAffiliateAllowedForCampaign($affiliateId, $campaignId, $checkDate = false)
@@ -221,6 +52,13 @@ class AW_Affiliate_Model_Api2_Check extends Mage_Api2_Model_Resource
     public function _isNewClientNotEqualCurrentClient($newClient, $currentClientId)
     {
         $client = Mage::getModel('awaffiliate/client')->load($currentClientId);
+        //var_dump($newClient->getCampaignId());
+        //var_dump($client->getCampaignId());
+        //var_dump($newClient->getAffiliateId());
+        //var_dump($client->getAffiliateId());
+        //var_dump($newClient->getCampaignId() != $client->getCampaignId() ||
+        //$newClient->getAffiliateId() != $client->getAffiliateId());
+        //die;
         if (
             $newClient->getCampaignId() != $client->getCampaignId() ||
             $newClient->getAffiliateId() != $client->getAffiliateId()
@@ -228,41 +66,5 @@ class AW_Affiliate_Model_Api2_Check extends Mage_Api2_Model_Resource
             return true;
         }
         return false;
-    }
-
-    public function beforeConfigurationSave($observer)
-    {
-        $controller = $observer->getControllerAction();
-        if ($controller->getRequest()->getParam('section') === 'awaffiliate') {
-            $previousMinimumWithdrawalPeriod = Mage::helper('awaffiliate/config')->getMinimumWithdrawalPeriod();
-            Mage::register(self::MINIMUM_WITHDRAWAL_PERIOD_REGISTRY_KEY, $previousMinimumWithdrawalPeriod);
-        }
-    }
-
-    public function afterConfigurationSave($observer)
-    {
-        if (($previousMinimumWithdrawalPeriod = Mage::registry(self::MINIMUM_WITHDRAWAL_PERIOD_REGISTRY_KEY)) !== null) {
-            $currentMinimumWithdrawalPeriod = Mage::helper('awaffiliate/config')->getMinimumWithdrawalPeriod();
-            if ($previousMinimumWithdrawalPeriod != $currentMinimumWithdrawalPeriod) {
-                // Invalidate Affiliates balance index
-                Mage::getSingleton('index/indexer')->getProcessByCode('awaffiliate_affiliate_balance')
-                    ->changeStatus(Mage_Index_Model_Process::STATUS_REQUIRE_REINDEX);
-            }
-        }
-    }
-
-    public function checkPrototype($observer)
-    {
-        if ((($block = $observer->getBlock()) instanceof Mage_Page_Block_Html_Head)
-            && (Mage::helper('awaffiliate')->isNewPrototypeRequired())
-        ) {
-            $items = $block->getData('items');
-            foreach ($items as $k => &$v) {
-                if (strcmp($v['name'], 'prototype/prototype.js') === 0) {
-                    $v['name'] = 'aw_affiliate/prototype.js';
-                }
-            }
-            $block->setData('items', $items);
-        }
     }
 }
--- app/code/local/AW/Affiliate/Model/Api2/Check/Rest/Admin/V1.php
+++ app/code/local/AW/Affiliate/Model/Api2/Check/Rest/Admin/V1.php
@@ -9,12 +9,16 @@
  */
 class AW_Affiliate_Model_Api2_Check_Rest_Admin_V1 extends AW_Affiliate_Model_Api2_Check_Rest
 {
+    //example path:
+    //http://www.bilnaclone.com/api/rest/affiliate/check?customer_id=95504&cmid=NWl6WlpHZ3FxVFk9&afid=dUMyemkrY1YyU2c9&ats=V0kwS2tCekNITU09&current_url=http%3A%2F%2Fwww.bilnaclone.com%2Fregal-marie-baru-klg-550g.html%3Fcmid%3DNWl6WlpHZ3FxVFk9%26afid%3DdUMyemkrY1YyU2c9%26ats%3DV0kwS2tCekNITU09
     protected function _retrieve()
     {
         $cmid = $this->getRequest()->getParam(AW_Affiliate_Helper_Affiliate::CAMPAIGN_REQUEST_KEY, null);
         $afid = $this->getRequest()->getParam(AW_Affiliate_Helper_Affiliate::AFFILIATE_REQUEST_KEY, null);
         $ats = $this->getRequest()->getParam(AW_Affiliate_Helper_Affiliate::AFFILIATE_TRAFFIC_SOURCE, null);
+        $customerId = $this->getRequest()->getParam('customer_id', null);
         $currentUrl = urldecode($this->getRequest()->getParam('current_url', null));
+        
         //var_dump($currentUrl);die;
         if (!is_null($cmid) && !is_null($afid) && !is_null($ats)) {
             //params detect
@@ -23,8 +27,11 @@ class AW_Affiliate_Model_Api2_Check_Rest_Admin_V1 extends AW_Affiliate_Model_Api
             $trafficSourceId = Mage::helper('core')->decrypt(Mage::helper('core')->urlDecode($ats));
             $cookieModel = Mage::getSingleton('core/cookie');
             $clientId = $cookieModel->get(AW_Affiliate_Helper_Config::COOKIE_NAME);
+            
+            $client = null;
+            $redirectUrl = null;
+            //var_dump($this->_isCampaignAccessible($campaignId));die;
             if (
-                $this->_isCookieFree($clientId) &&
                 $this->_isAffiliateAccessible($affiliateId) &&
                 $this->_isCampaignAccessible($campaignId) &&
                 $this->_isTrafficSourceAvailable($trafficSourceId) &&
@@ -35,31 +42,53 @@ class AW_Affiliate_Model_Api2_Check_Rest_Admin_V1 extends AW_Affiliate_Model_Api
                     'campaign_id' => $campaignId,
                     'affiliate_id' => $affiliateId,
                     'traffic_id' => $trafficSourceId,
-                    'customer_id' => Mage::getSingleton('customer/session')->getId()
+                    'customer_id' => $customerId
                 ));
+                
+                //var_dump($clientId);
+                //var_dump($newClient->getId());
+                //var_dump($this->_isNewClientNotEqualCurrentClient($newClient, $clientId));die;
+                
                 if ($this->_isNewClientNotEqualCurrentClient($newClient, $clientId)) {
                     try {
                         $newClient->save();
-                        $cookieModel->set(AW_Affiliate_Helper_Config::COOKIE_NAME, $newClient->getId(), true); //set cookie on one year
+                        $client = array(
+                            'id' => $newClient->getId(), 
+                            'campaign_id' => $campaignId,
+                            'affiliate_id' => $affiliateId,
+                            'traffic_id' => $trafficSourceId,
+                            'customer_id' => $customerId
+                        ); 
                     } catch (Exception $e) {
+                        Mage::throwException($e->getMessage());
                     }
+                } else {
+                    $client = array(
+                        'id' => $clientId, 
+                        'campaign_id' => $campaignId,
+                        'affiliate_id' => $affiliateId,
+                        'traffic_id' => $trafficSourceId,
+                        'customer_id' => $customerId
+                    ); 
                 }
+                
+                $redirectUrl = Mage::helper('awaffiliate')->removeRequestParam($currentUrl, AW_Affiliate_Helper_Affiliate::CAMPAIGN_REQUEST_KEY);
+                $redirectUrl = Mage::helper('awaffiliate')->removeRequestParam($redirectUrl, AW_Affiliate_Helper_Affiliate::AFFILIATE_REQUEST_KEY);
+                $redirectUrl = Mage::helper('awaffiliate')->removeRequestParam($redirectUrl, AW_Affiliate_Helper_Affiliate::AFFILIATE_TRAFFIC_SOURCE);
             }
-            //redirect
-            //$currentUrl = Mage::helper('core/url')->getCurrentUrl();
-            $redirectUrl = Mage::helper('awaffiliate')->removeRequestParam($currentUrl, AW_Affiliate_Helper_Affiliate::CAMPAIGN_REQUEST_KEY);
-            $redirectUrl = Mage::helper('awaffiliate')->removeRequestParam($redirectUrl, AW_Affiliate_Helper_Affiliate::AFFILIATE_REQUEST_KEY);
-            $redirectUrl = Mage::helper('awaffiliate')->removeRequestParam($redirectUrl, AW_Affiliate_Helper_Affiliate::AFFILIATE_TRAFFIC_SOURCE);
             
             return array(
                 'params' => array(
                     AW_Affiliate_Helper_Affiliate::CAMPAIGN_REQUEST_KEY => $cmid, 
                     AW_Affiliate_Helper_Affiliate::AFFILIATE_REQUEST_KEY => $afid, 
-                    AW_Affiliate_Helper_Affiliate::AFFILIATE_TRAFFIC_SOURCE => $ats,
-                    'redirect_url' => $redirectUrl
+                    AW_Affiliate_Helper_Affiliate::AFFILIATE_TRAFFIC_SOURCE => $ats
                 ), 
+                'client' => $client, 
+                'redirect_url' => $redirectUrl
             );
         }
+        
+        Mage::throwException('Pleas provide all parameter to process affiliate.');
     }
 
 }
