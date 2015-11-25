8852b705eb5f8e42bea0da4063961c76f4ce5e88
denver <denver@bilna.com>
2015-11-25 18:29:41 +0700

    Universal Analytics (sku)

--- app/code/local/AW/Featured/Block/Representations/Common.php
+++ app/code/local/AW/Featured/Block/Representations/Common.php
@@ -286,7 +286,7 @@ class AW_Featured_Block_Representations_Common extends Mage_Core_Block_Template
             $this->_collection->joinOveralRating();
             $this->_collection->joinReviewsCount();
             $attr = array(
-                'name', 'short_description', 'small_image', 'thumbnail', 'image',
+                'name', 'short_description', 'small_image', 'thumbnail', 'image', 'sku',
                 'msrp', 'msrp_enabled', 'msrp_display_actual_price_type', 'aw_os_category_display',
 
                 'aw_os_category_position', 'aw_os_category_image', 'aw_os_category_image_path', 'aw_os_category_text','news_from_date','news_to_date'
