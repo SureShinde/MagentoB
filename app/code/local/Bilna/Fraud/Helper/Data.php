ac1ab45773b8ff54148ed15c230a26c1ca90c70c
harfil <harfil@bilna.com>
2015-11-25 16:14:56 +0700

    wrong if logic

--- app/code/local/Bilna/Fraud/Helper/Data.php
+++ app/code/local/Bilna/Fraud/Helper/Data.php
@@ -8,7 +8,7 @@
 class Bilna_Fraud_Helper_Data extends Mage_Core_Helper_Abstract {
     public function checkOrderStatus($orderId, $loadByIncrement = 0) {
         $canceled = 0;
-        if($loadByIncrement == 0) {
+        if($loadByIncrement == 1) {
             $orderData = Mage::getModel('sales/order')->loadByIncrementId($orderId);
         }
         else {
