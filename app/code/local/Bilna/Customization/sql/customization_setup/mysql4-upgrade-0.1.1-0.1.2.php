4538b1bac43d3e67ae6450535483d30751db44f4
halimindra <indra.halim@bilna.com>
2015-11-26 17:30:50 +0700

    BILNA-1391: Added new EAV Attribute for Catalog Category

---
+++ app/code/local/Bilna/Customization/sql/customization_setup/mysql4-upgrade-0.1.1-0.1.2.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @author Indra Halim
+ * Penambahan field include_in_megamenu di EAV untuk Catalog Category
+ */
+$installer = $this;
+$installer->startSetup();
+$installer->addAttribute('catalog_category', 'include_in_megamenu', array (
+    'group' => 'General Information',
+    'type' => 'int',
+    'input' => 'select',
+    'source'  => 'eav/entity_attribute_source_boolean',
+    'label' => 'Include in Mega Menu',
+    'required' => true,
+    'default' => 1,
+    'global' => Mage_Catalog_Model_Resource_Eav_Attribute::SCOPE_STORE,
+));     
+$installer->endSetup();
