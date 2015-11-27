fdd6b42cd193ec2c86b0c7c26a94f1bdc8d05b42
halimindra <indra.halim@bilna.com>
2015-11-27 14:36:19 +0700

    BILNA-1391: Penambahan mekanisme populate data awal untuk include_in_megamenu

--- app/code/local/Bilna/Customization/sql/customization_setup/mysql4-upgrade-0.1.1-0.1.2.php
+++ app/code/local/Bilna/Customization/sql/customization_setup/mysql4-upgrade-0.1.1-0.1.2.php
@@ -15,4 +15,15 @@ $installer->addAttribute('catalog_category', 'include_in_megamenu', array (
     'default' => 0,
     'global' => Mage_Catalog_Model_Resource_Eav_Attribute::SCOPE_STORE,
 ));
+
+//Populate value attribute di catalog/category
+$categoryModel = Mage::getModel('catalog/category');
+$catTree = $categoryModel->getTreeModel()->load();
+$catIds = $catTree->setStoreId(0)->getCollection()->getAllIds();
+foreach($catIds as $id){
+    $categoryModel->setId($id);
+    $categoryModel->setIncludeInMegamenu(1);
+    $categoryModel->save();
+}
+
 $installer->endSetup();
