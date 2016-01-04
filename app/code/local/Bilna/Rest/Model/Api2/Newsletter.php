5da899b4612bd37a45ca2fdb25a64d65d5bd8791
Dhany Noor Alfian <dhanyalvian@gmail.com>
2016-01-04 18:40:37 +0700

    API Newsletter - add api subscribe newsletter

---
+++ app/code/local/Bilna/Rest/Model/Api2/Newsletter.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Description of Bilna_Rest_Model_Api2_Newsletter
+ *
+ * @author Bilna Development Team <development@bilna.com>
+ */
+
+abstract class Bilna_Rest_Model_Api2_Newsletter extends Bilna_Rest_Model_Api2 {
+    
+}
