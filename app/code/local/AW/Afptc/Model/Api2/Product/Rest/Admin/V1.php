0b3f0b17340f8e2ea3fe6d77235613620bc5f0ba
M. KHAIRUL AZAMI, S. KOM <khairul.azami@bilna.com>
2016-01-11 19:01:40 +0700

    bug fix accepted free product API

--- app/code/local/AW/Afptc/Model/Api2/Product/Rest/Admin/V1.php
+++ app/code/local/AW/Afptc/Model/Api2/Product/Rest/Admin/V1.php
@@ -15,6 +15,35 @@ class AW_Afptc_Model_Api2_Product_Rest_Admin_V1 extends AW_Afptc_Model_Api2_Prod
      * @param int|string $store
      * @return int
      */
+    protected function _retrieve()
+    {
+        $productId = (int) $this->getRequest()->getParam('product_id');
+    	$quoteId = (int) $this->getRequest()->getParam('quote_id');
+    	$qty = (int) $this->getRequest()->getParam('qty');
+    	$ruleId = (int) $this->getRequest()->getParam('rule_id');
+
+    	try{
+    		$quote = $this->_getQuote($quoteId);
+    		$rule = Mage::getModel('awafptc/rule')->load($ruleId);
+
+    		if( $productId == $rule->getProductId() )
+    		{
+    			$product = Mage::getModel('catalog/product')->load($rule->getProductId());
+    			if (!$product->getId())
+    				throw Mage::throwException("Product with ID $productId doesn\'t exists");
+
+    			$quote->addProduct($product->setData('aw_afptc_rule', $rule))->setQty($qty);
+    		}
+
+    	}catch(Exception $e){
+    		$this->_error($e->getMessage(), Mage_Api2_Model_Server::HTTP_INTERNAL_ERROR);
+    	}
+        
+        return $quote;
+
+    	//return $this->_getLocation($quote);
+    }
+    /*
     protected function _create(array $data)
     {
     	$productId = (int) $data['product_id'];
@@ -41,5 +70,5 @@ class AW_Afptc_Model_Api2_Product_Rest_Admin_V1 extends AW_Afptc_Model_Api2_Prod
 
     	return $this->_getLocation($quote);
     }
-
+    */
 }
