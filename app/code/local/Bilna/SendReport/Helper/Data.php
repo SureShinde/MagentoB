fcbc68fe0df71aedfd55b134219a6ab9aae7840a
indrakbil <indra.kurniawan@bilna.com>
2016-01-25 14:19:32 +0700

    set configuration sender and receiver from database

--- app/code/local/Bilna/SendReport/Helper/Data.php
+++ app/code/local/Bilna/SendReport/Helper/Data.php
@@ -8,16 +8,6 @@ class Bilna_SendReport_Helper_Data extends Mage_Core_Helper_Abstract {
 	    return $matches[1];
 	}
 
-    public function getCollectionByCode($code){
-    	$sendreport = Mage::getModel('sendreport/salescategory');
-	    $coll = $sendreport->getCollection()
-				->addFieldToFilter('send_report_code', $code)
-				->addFieldToFilter('status', 1)
-				->setPageSize(1)
-			    ->setCurPage(1);
-		return $coll;
-    }
-
     public function getData($sql){
     	$read = Mage::getSingleton('core/resource')->getConnection('core_read');
 		return $read->query($sql);
