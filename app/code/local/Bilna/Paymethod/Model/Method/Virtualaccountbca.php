30191b144b486f922005fd5f852301814d3a271c
halimindra <indra.halim@bilna.com>
2016-02-05 11:33:56 +0700

    add skeleton for virtual account BCA payment method

---
+++ app/code/local/Bilna/Paymethod/Model/Method/Virtualaccountbca.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Description of Bilna_Paymethod_Model_Method_Mandiriecash
+ *
+ * @author Bilna Development Team <development@bilna.com>
+ */
+
+class Bilna_Paymethod_Model_Method_Virtualaccountbca extends Mage_Payment_Model_Method_Banktransfer {
+    protected $_code = 'virtualaccountbca';
+//    protected $_formBlockType = 'paymethod/form_mandiriecash';
+//    protected $_infoBlockType = 'paymethod/info_mandiriecash';
+}
