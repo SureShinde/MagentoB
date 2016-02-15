5fe3535acfdd82e83fd4c671f5f7bb45b1525d44
Taufik Ramdani <taufik.r@bilna.com>
2016-02-15 17:04:00 +0700

    adding moxy payment method history for data migration

---
+++ app/code/local/Bilna/Paymethod/Model/Method/Moxyveritrans.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Description of Bilna_Paymethod_Model_Method_Moxyveritrans
+ *
+ * @author Bilna Development Team <development@bilna.com>
+ */
+
+class Bilna_Paymethod_Model_Method_Moxyveritrans extends Bilna_Paymethod_Model_Method_Vtdirect {
+    protected $_code = 'moxyveritrans';
+}
