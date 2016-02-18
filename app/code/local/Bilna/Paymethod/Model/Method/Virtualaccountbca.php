32d9a3a248dcb1fa84d864992361bf7fa846c240
halimindra <indra.halim@bilna.com>
2016-02-18 15:41:13 +0700

    BILNA-1453: add info block for front end

--- app/code/local/Bilna/Paymethod/Model/Method/Virtualaccountbca.php
+++ app/code/local/Bilna/Paymethod/Model/Method/Virtualaccountbca.php
@@ -6,5 +6,22 @@
  */
 
 class Bilna_Paymethod_Model_Method_Virtualaccountbca extends Mage_Payment_Model_Method_Banktransfer {
+    /**
+     * Payment method code
+     *
+     * @var string
+     */
     protected $_code = 'virtualaccountbca';
+
+//    protected $_formBlockType = 'payment/form_banktransfer';
+    protected $_infoBlockType = 'payment/info_virtualaccountbca';
+
+    /**
+     * Get instructions text from config
+     *
+     * @return string
+     */
+    public function getInstructions() {
+        return trim($this->getConfigData('instructions'));
+    }
 }
