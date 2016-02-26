065ad700a0aec169518fe5c0c2db8655a3640862
halimindra <indra.halim@bilna.com>
2016-02-26 15:12:49 +0700

    ORAMI-65: add flag express_shipping on quote_item and order_item for front end

--- app/code/local/Bilna/Expressshipping/sql/expressshipping_setup/mysql4-upgrade-0.1.0-0.1.1.php
+++ app/code/local/Bilna/Expressshipping/sql/expressshipping_setup/mysql4-upgrade-0.1.0-0.1.1.php
@@ -3,6 +3,7 @@
 $installer = $this;
 $installer->startSetup();
 
-$installer->run("ALTER TABLE `{$this->getTable('sales_flat_order_item')}` ADD COLUMN `is_express_shipping` INT(11) DEFAULT 0");
+$installer->run("ALTER TABLE `{$this->getTable('sales_flat_order_item')}` ADD COLUMN `express_shipping` INT(11) DEFAULT 0");
+$installer->run("ALTER TABLE `{$this->getTable('sales_flat_quote_item')}` ADD COLUMN `express_shipping` INT(11) DEFAULT 0");
 
 $installer->endSetup();
