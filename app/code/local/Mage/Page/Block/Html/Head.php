58c15adc83a1711dd3b3aad50cd6470d3757ad54
I Made Raka Teja <raka.teja@bilna.com>
2016-03-01 17:23:01 +0700

    #ORAMI-92 Implement wishlist cover image on facebook/twitter shared

--- app/code/local/Mage/Page/Block/Html/Head.php
+++ app/code/local/Mage/Page/Block/Html/Head.php
@@ -593,4 +593,14 @@ class Mage_Page_Block_Html_Head extends Mage_Core_Block_Template
         return $this->getData('post');
     }
         /*End Blog Function adding by andi*/
+
+    public function setWishlist($wishlist)
+    {
+        $this->_data['wishlist'] = $wishlist;
+    }
+
+    public function getWishlist()
+    {
+        return $this->_data['wishlist'];
+    }
 }
