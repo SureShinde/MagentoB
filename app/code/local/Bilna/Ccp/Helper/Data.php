062cfc146799b7d9796e2b40cf8749cb1cf4b359
RudyHalim <rudy.halim@bilna.com>
2016-03-02 12:04:18 +0700

    updated model

---
+++ app/code/local/Bilna/Ccp/Helper/Data.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Description of Bilna_Ccp_Helper_Data
+ *
+ * @author Bilna Development Team <development@bilna.com>
+ */
+
+class Bilna_Ccp_Helper_Data extends Mage_Core_Helper_Abstract {
+    
+}
--- app/code/local/Bilna/Ccp/Model/Ccpmodel.php
+++ app/code/local/Bilna/Ccp/Model/Ccpmodel.php
@@ -24,6 +24,9 @@ class Bilna_Ccp_Model_Ccpmodel  extends Mage_Core_Model_Abstract {
 
 		$resource = new Mage_Core_Model_Resource();  
 		$read = $resource->getConnection('core_read');  
+
+		$formatted_orderstatus = '"'.implode('","', explode(",", $configValues['order_status'])).'"';
+
 		$select = $read->select()
 		               ->from(array('main_table' => 'catalog_product_flat_1'),
 		               			array('main_table.name'))
@@ -43,16 +46,50 @@ class Bilna_Ccp_Model_Ccpmodel  extends Mage_Core_Model_Abstract {
 						   , array('status')
 						)
 		               ->columns('sum(sales_item.qty_ordered*sales_item.price) as revenue')
-		               ->where('sales_order.status IN ("complete", "processing", "processing_cod", "shipping_cod", "holded")')
-		               ->where('sales_item.product_type != ?', 'bundle')
+		               ->where('sales_order.status IN ('.$formatted_orderstatus.')')
+		               ->where($configValues['product_bundle'] ? '1=1' : 'sales_item.product_type != ?', 'bundle')
 		               ->where('sales_order.created_at BETWEEN (NOW() - INTERVAL '.$configValues['max_days'].' DAY) AND NOW() ')
-		               ->limit(8)
 		               ->group('sales_item.product_id')
 		            ;
 
 		// Mage::log((string)$select);
 
 		return $read->fetchAll($select);  
+
+
+     	// $collections = $this->getCollection();        
+      //   $collections->getSelect()->reset(Zend_Db_Select::COLUMNS)->joinLeft(
+      //           array('stock' => Mage::getConfig()->getTablePrefix()."cataloginventory_stock_item")
+      //           , 'stock.product_id = main_table.entity_id'
+      //           , array('main_table.name', 'is_in_stock')
+      //       );        
+      //   $collections->getSelect()->joinLeft(
+      //           array('sales_item' => Mage::getConfig()->getTablePrefix()."sales_flat_order_item")
+      //           , 'sales_item.product_id = main_table.entity_id '
+      //           , array('product_id')
+      //       );
+      //   $collections->getSelect()->joinLeft(
+      //           array('sales_order' => Mage::getConfig()->getTablePrefix()."sales_flat_order")
+      //           , 'sales_item.order_id = sales_order.entity_id'
+      //           , array('status')
+      //       );
+      //   $collections->getSelect()->columns('stock.qty as stock_qty, sum(sales_item.qty_ordered*sales_item.price) as revenue');
+      //   $collections->getSelect()->columns(
+      //       array(
+      //           'categories' => new Zend_Db_Expr("(select concat(';',(GROUP_CONCAT(category.category_id SEPARATOR ';')),';') from catalog_category_product_index category where category.product_id = main_table.entity_id and store_id=1)")
+      //       ));
+
+      //   $collections->addFieldToFilter('sales_order.status', array('in' => array('complete', 'processing', 'processing_cod', 'shipping_cod', 'holded')));
+      //   $collections->addFieldToFilter('sales_item.product_type', array('neq' => 'bundle'));
+      //   $collections->addFieldToFilter('sales_order.created_at', array(
+      //                                       'from'     => strtotime('-30 day', time()),
+      //                                       'to'       => time(),
+      //                                       'datetime' => true
+      //                                   )
+      //                               );
+      //   $collections->getSelect()->group('sales_item.product_id');
+
+      //   return $collections->getData();
     }
 
     public function setRankings($field_to_compare, $arr_source = array()) {
