932d425301424fb81eb7cf8766780d0238a7d6a5
RudyHalim <rudy.halim@bilna.com>
2016-03-03 17:19:49 +0700

    fixed query for rjm

--- app/code/local/Bilna/Ccp/Model/Ccpmodel.php
+++ app/code/local/Bilna/Ccp/Model/Ccpmodel.php
@@ -4,45 +4,88 @@ class Bilna_Ccp_Model_Ccpmodel extends Mage_Core_Model_Abstract {
     protected function _construct() {
         $this->_init('ccp/ccpmodel');
     }
-
-    public function getMatchedProductsList($param = array()) {
-        $configValues = Mage::getStoreConfig('bilna_ccp/ccp');
+    
+    // output: array { [0] => array {'name' => "ABCD 2 IN 1", 'product_id' => "31905", 'stock_qty' => "0.0000" }
+    public function getProductInventories($param = array()) {
         $resource = new Mage_Core_Model_Resource();  
         $read = $resource->getConnection('core_read');  
-        $formatted_orderstatus = '"'.implode('","', explode(",", $configValues['order_status'])).'"';
 
         $select = $read->select()
-        ->from(array('main_table' => 'catalog_product_flat_1'),
-            array('main_table.name'))
-        ->joinLeft(
-            array('stock' => Mage::getConfig()->getTablePrefix()."cataloginventory_stock_item")
-            , 'stock.product_id = main_table.entity_id'
-            , array('stock.is_in_stock', 'stock.qty as stock_qty')
-            )
-        ->joinLeft(
-            array('sales_item' => Mage::getConfig()->getTablePrefix()."sales_flat_order_item")
-            , 'sales_item.product_id = main_table.entity_id '
-            , array('product_id')
-            )
-        ->joinLeft(
-            array('sales_order' => Mage::getConfig()->getTablePrefix()."sales_flat_order")
-            , 'sales_item.order_id = sales_order.entity_id'
-            , array('status')
-            )
-        ->columns('sum(sales_item.qty_ordered*sales_item.price) as revenue')
-        ->where('sales_order.status IN ('.$formatted_orderstatus.')')
-        ->where($configValues['product_bundle'] ? '1=1' : 'sales_item.product_type != ?', 'bundle')
-        ->where('sales_order.created_at >= NOW() - INTERVAL '.$configValues['max_days'].' DAY ')
-        ->group('sales_item.product_id')
-        ;
-        return $read->fetchAll($select);  
+            ->from(array('main_table' => 'catalog_product_flat_1'),
+                array('main_table.name', 'main_table.entity_id as product_id'))
+            ->joinLeft(
+                array('stock' => Mage::getConfig()->getTablePrefix()."cataloginventory_stock_item")
+                , 'stock.product_id = main_table.entity_id'
+                , array('stock.qty as stock_qty')
+                )
+            ;
+        Mage::log((string)$select);
+        $product_stock = $read->fetchAll($select);
+        return $product_stock;
+    }
+
+    // output: array { [0] => array {'name' => "ABCD 2 IN 1", 'product_id' => "31905", 'stock_qty' => "0.0000", 'sales' => "6741500.00000000" }
+    public function getProductsSales($product_stock) {
+        $configValues = Mage::getStoreConfig('bilna_ccp/ccp'); 
+        $resource = new Mage_Core_Model_Resource();  
+        $read = $resource->getConnection('core_read'); 
+
+        $productQtyArray = array();
+        foreach ($product_stock as $key => $value) {
+            $select = $read->select()
+                ->from(array('main_table' => 'sales_flat_order_item'), array())
+                ->columns('sum(qty_ordered*price) as sales')
+                ->where($configValues['product_bundle'] ? '1=1' : 'product_type != ?', 'bundle')
+                ->where('product_id=?', $value['product_id'])
+                ->group('product_id')
+                ;
+            Mage::log((string)$select);
+            $product_sales = $read->fetchAll($select);
+            // output: array { [0] => array {'sales' => "6741500.00000000" } }
+
+            $product_stock[$key]['sales'] = sizeof($product_sales) > 0 && $product_sales[0]['sales'] ? (int)$product_sales[0]['sales'] : 99999;
+        }
+        return $product_stock;
+    }
+
+    public function setProductScoringDataTable($product_stock) {
+        // do the VM routines here
+        $arr_sales_rank = $this->setRankings('sales', $product_stock);
+        $arr_inv_rank = $this->setRankings('stock_qty', $product_stock);
+        $configValues = Mage::getStoreConfig('bilna_ccp/ccp');
+        $percentage_item = $configValues['percentage_itemsold']/100;
+        $percentage_inventory = $configValues['percentage_inventory']/100;
+
+        $write = Mage::getSingleton('core/resource')->getConnection('core_write');
+        $write->delete("bilna_ccp_product_scoring");
+
+        foreach ($product_stock as $key => $value) {
+            $product_id = $value['product_id'];
+            $score = $percentage_item*$arr_sales_rank[$product_id] + $percentage_inventory*$arr_inv_rank[$product_id];
+
+            try {
+                $write->insert(
+                        "bilna_ccp_product_scoring", 
+                        array('product_id' => $product_id,
+                                'sales' => (int)$value['sales'],
+                                'sales_rank' => $arr_sales_rank[$product_id],
+                                'inventory' => $value['stock_qty'],
+                                'inventory_rank' => $arr_inv_rank[$product_id],
+                                'score' => $score
+                        )
+                );
+                Mage::log('Product ID '.$product_id.' ('.$arr_sales_rank[$product_id].'|'.$arr_inv_rank[$product_id].') has been updated with score of '.$score);
+            } catch(Exception $e) {
+                Mage::logException($e);
+            }
+        }
     }
 
     /* input array format: 
     {
-      [0] => {'name' => "Pampers Active Baby Pants Diapers M 56",'is_in_stock' => "1",'stock_qty' => "306.0000",'product_id' => "268",'status' => "processing",'revenue' => "1188000.00000000", },
-      [1] => {'name' => "Sebamed Baby Care Cream 100ml",'is_in_stock' => "1",'stock_qty' => "1.0000",'product_id' => "320",'status' => "processing",'revenue' => "107000.00000000",  },
-      [2] => {'name' => "LG Smartphone L1 II Black",'is_in_stock' => "1",'stock_qty' => "-9.0000",'product_id' => "52508",'status' => "processing",'revenue' => "733000.00000000",  }
+      [0] => {'name' => "Pampers Active Baby Pants Diapers M 56",'is_in_stock' => "1",'stock_qty' => "306.0000",'product_id' => "268",'status' => "processing",'sales' => "1188000.00000000", },
+      [1] => {'name' => "Sebamed Baby Care Cream 100ml",'is_in_stock' => "1",'stock_qty' => "1.0000",'product_id' => "320",'status' => "processing",'sales' => "107000.00000000",  },
+      [2] => {'name' => "LG Smartphone L1 II Black",'is_in_stock' => "1",'stock_qty' => "-9.0000",'product_id' => "52508",'status' => "processing",'sales' => "733000.00000000",  }
     }
     */
     public function setRankings($field_to_compare, $productArray) {     
@@ -50,7 +93,7 @@ class Bilna_Ccp_Model_Ccpmodel extends Mage_Core_Model_Abstract {
         if(sizeof($productArray) > 0 ) {
             foreach ($productArray as $key => $value) {
                 // we use product id as key for unique mapping
-                $result[$value['product_id']] = $value[$field_to_compare];
+                $result[$value['product_id']] = (int)$value[$field_to_compare];
             }
             $result = $this->calculateRank($result);
         }       
@@ -58,9 +101,19 @@ class Bilna_Ccp_Model_Ccpmodel extends Mage_Core_Model_Abstract {
     }
 
     // input array format: {[268] => "306.0000", [320] => "1.0000", [52508] => "-9.0000"}
-    public function calculateRank($array) {
+    // output depends on sorting: {[268] => "1", [320] => "2", [52508] => "3"}
+    public function calculateRank($array, $sorting="rsort") {
         $arr_sorted = $array;
-        rsort($arr_sorted);
+
+        switch ($sorting) {
+            case 'sort':
+                sort($arr_sorted);
+                break;            
+            default:
+                rsort($arr_sorted);
+                break;
+        }
+        
         $arr_sorted = array_flip($arr_sorted);
         foreach($array as $key => $val)
             $arr_result[$key] = $arr_sorted[$val]+1;
--- shell/bilna/generateProductScoring.php
+++ shell/bilna/generateProductScoring.php
@@ -4,40 +4,15 @@ require_once dirname(__FILE__) . '/../abstract.php';
 class Ccp extends Mage_Shell_Abstract {
     
     public function run() {
-
+        // pull products list as RJM does
         $productModel = Mage::getModel('ccp/ccpmodel');
-        $productsList = $productModel->getMatchedProductsList();
 
-        $arr_sales_rank = $productModel->setRankings('revenue', $productsList);
-        $arr_inv_rank = $productModel->setRankings('stock_qty', $productsList);
+        $productsList = $productModel->getProductInventories();
+        $productsList = $productModel->getProductsSales($productsList);
+        $productsList = $productModel->setProductScoringDataTable($productsList);
 
-        $configValues = Mage::getStoreConfig('bilna_ccp/ccp');
-        $percentage_item = $configValues['percentage_itemsold']/100;
-        $percentage_inventory = $configValues['percentage_inventory']/100;
-
-        $write = Mage::getSingleton('core/resource')->getConnection('core_write');
-        $write->delete("bilna_ccp_product_scoring");
-
-        foreach ($productsList as $item) {
-            $score = $percentage_item*$arr_sales_rank[$item['product_id']] + $percentage_inventory*$arr_inv_rank[$item['product_id']];
-
-            try {
-                $write->insert(
-                        "bilna_ccp_product_scoring", 
-                        array('product_id' => $item['product_id'],
-                                'sales' => $item['revenue'],
-                                'sales_rank' => $arr_sales_rank[$item['product_id']],
-                                'inventory' => $item['stock_qty'],
-                                'inventory_rank' => $arr_inv_rank[$item['product_id']],
-                                'score' => $score,
-                                'created_at' => now()
-                        )
-                );
-                Mage::log("CCP was successfully updated.");
-            } catch(Exception $e) {
-                Mage::logException($e);
-            }
-        }        
+        // now insert to CCP table
+        echo $productModel->setProductPosition();
     }    
 }
 
