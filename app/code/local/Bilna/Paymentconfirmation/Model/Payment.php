2c5f2370d8dbdbf3b30380a9c565782446464d7b
dendhi31 <dendhi31@yahoo.com>
2016-03-10 13:11:20 +0700

    remove query from model

--- app/code/local/Bilna/Paymentconfirmation/Model/Payment.php
+++ app/code/local/Bilna/Paymentconfirmation/Model/Payment.php
@@ -18,15 +18,6 @@ class Bilna_Paymentconfirmation_Model_Payment extends Mage_Core_Model_Abstract{
         return $order;
     }
     
-    public function isPaymentExists($orderNumber){
-        $db = Mage::getSingleton('core/resource')->getConnection('core_read');
-        $field = array("order_id"=>"order_id","email"=>"email","nominal"=>"nominal",
-                       "dest_bank"=>"dest_bank","transfer_date"=>"transfer_date",
-                       "source_bank"=>"source_bank","source_acc_number"=>"source_acc_number",
-                       "source_acc_name"=>"source_acc_name","comment"=>"comment","entity_id"=>"entity_id");
-        $query = $db->select()->from($this->confirmationTable,$field)->where(sprintf("order_id = %s",$orderNumber))->order('transfer_date','DESC')->limit(1);
-	return $db->fetchAll($sql);
-    }
     
 }
 
