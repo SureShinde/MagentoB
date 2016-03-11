fc6e0ec5a9f1e4a4f03e5ad4e35815d740c93e13
dendhi31 <dendhi31@yahoo.com>
2016-03-11 11:34:07 +0700

    use magento standart model change cron temporary filename from harcode to autogenerate change cron date function to magento date function change cron to run every hour

--- cron/PaymentConfirmationReport.php
+++ cron/PaymentConfirmationReport.php
@@ -9,16 +9,17 @@
     $i = 1;
     $oldId = 0;
     $stop = 0;
-    $filename = './tes.csv';
+    $filename = rand(1000,9999).'.csv';
     //print $filename;
     $handle = fopen($filename,'w+');
     //print $handle;exit;
+    $date = Mage::getModel('core/date')->date('Y-m-d 00:00:00');
     fwrite($handle,"ORDER ID,e-mail,Nominal,Bank Penerima,Tanggal Transfer,Bank Pengirim,No Rek Pengirim,Nama Pengirim,Komentar\n");
     while(true){
         $sendMail = Mage::getModel('Paymentconfirmation/payment')
                 ->getCollection()
-                ->addFieldToFilter('created_at',array('gteq'=>date('Y-m-d H:i:s',mktime(0,0,0,date('m'),intval(date('d'))-1,date('Y')))))
-                ->addFieldToFilter('created_at',array('lteq'=>date('Y-m-d H:i:s',mktime(23,59,59,date('m'),intval(date('d'))-1,date('Y')))))
+                ->addFieldToFilter('created_at',array('gteq'=>Mage::getModel('core/date')->date('Y-m-d H:00:00', strtotime($date." -1 hours"))))
+                ->addFieldToFilter('created_at',array('lteq'=>Mage::getModel('core/date')->date('Y-m-d H:59:59', strtotime($date." -1 hours"))))
                 ->setCurPage($i)
                 ->setPageSize(10);
         if(count($sendMail) < 1) break;
@@ -43,13 +44,13 @@
     $mail->setBodyHtml($html);
     $mail->setFrom(Mage::getStoreConfig('bilna_paymentconfirmation/paymentconfirmation/sender_email'),Mage::getStoreConfig('bilna_paymentconfirmation/paymentconfirmation/sender_name'));
     $mail->addTo(Mage::getStoreConfig('bilna_paymentconfirmation/paymentconfirmation/receiver_email'),Mage::getStoreConfig('bilna_paymentconfirmation/paymentconfirmation/receiver_name'));
-    $mail->setSubject('[BILNA] Payment Confirmation List '.date('Y-m-d',mktime(0,0,0,date('m'),intval(date('d')-1),date('Y'))));
+    $mail->setSubject('[BILNA] Payment Confirmation List '.Mage::getModel('core/date')->date('Y-m-d H', strtotime($date." -1 hours")));
     $dir = Mage::getBaseDir();
     $file = $mail->createAttachment(file_get_contents($filename));
     $file ->type        = 'text/csv';
     $file ->disposition = Zend_Mime::DISPOSITION_INLINE;
     $file ->encoding    = Zend_Mime::ENCODING_BASE64;
-    $file ->filename    = sprintf('payment_confirmation_list_%s.csv',date('Y-m-d',mktime(0,0,0,date('m'),intval(date('d')-1),date('Y'))));
+    $file ->filename    = sprintf('payment_confirmation_list_%s.csv',Mage::getModel('core/date')->date('YmdH', strtotime($date." -1 hours")));
     $mail->send();
     @unlink($filename);
     //$sendMail->cronEmailPaymentconfirmation();
