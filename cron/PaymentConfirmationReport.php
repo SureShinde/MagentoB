7b54ebb066a81bbf1d0aa9b4273ae5deb70eb531
dendhi31 <dendhi31@yahoo.com>
2016-03-22 10:41:29 +0700

    add confirmation time to file. Cron schedule time can be configured in config

--- cron/PaymentConfirmationReport.php
+++ cron/PaymentConfirmationReport.php
@@ -4,8 +4,23 @@
      */
 
     require_once realpath(dirname(__FILE__)).'/../app/Mage.php'; // load magento API
-
     Mage::app();
+    $configScheduled = Mage::getStoreConfig('bilna_paymentconfirmation/paymentconfirmation/run_time');
+    print "Scheduled Time: ".$configScheduled."\n";
+    print "Start Time: ".Mage::getStoreConfig('bilna_paymentconfirmation/paymentconfirmation/next_execute').":00:00"."\n";
+    print "End Time: ".Mage::getModel('core/date')->date('Y-m-d H:59:59', strtotime(" -1 hours"))."\n";
+    $arrScheduledTime = explode(",",$configScheduled);
+    print_r($arrScheduledTime);
+    $currentHour = (int)Mage::getModel('core/date')->date('H');
+    print "Jam Sekarang: ".$currentHour."\n";
+    if(in_array($currentHour,$arrScheduledTime)){
+        print "Waktunya jalan\n";
+    }
+    else{
+        print "Bukan Waktunya jalan\n";
+        $exitStatus = 0;
+        exit($exitStatus);
+    }
     $i = 1;
     $oldId = 0;
     $stop = 0;
@@ -13,6 +28,7 @@
     $csv = new Varien_File_Csv();
     $csvdata = array();
     $date = Mage::getModel('core/date')->date('Y-m-d H:00:00');
+    $data['created_at'] = "Waktu Konfirmasi";
     $data['number'] = "Nomor Pesanan";
     $data['email'] = "Alamat Email";
     $data['payTotal'] = "Jumlah Yang diBayar";
@@ -25,7 +41,7 @@
     while(true){
         $sendMail = Mage::getModel('Paymentconfirmation/payment')
                 ->getCollection()
-                ->addFieldToFilter('created_at',array('gteq'=>Mage::getModel('core/date')->date('Y-m-d H:00:00', strtotime(" -1 hours"))))
+                ->addFieldToFilter('created_at',array('gteq'=>Mage::getModel('core/date')->date(Mage::getStoreConfig('bilna_paymentconfirmation/paymentconfirmation/next_execute').":00:00")))
                 ->addFieldToFilter('created_at',array('lteq'=>Mage::getModel('core/date')->date('Y-m-d H:59:59', strtotime(" -1 hours"))))
                 ->setCurPage($i)
                 ->setPageSize(100);
@@ -38,6 +54,7 @@
                 break;
             }
             $data = array();
+            $data['created_at'] = $collection->created_at;
             $data['number'] = $collection->order_id;
             $data['email'] = $collection->email;
             $data['payTotal'] = $collection->nominal;
@@ -62,13 +79,13 @@
     $mail->setFrom(Mage::getStoreConfig('bilna_paymentconfirmation/paymentconfirmation/sender_email'),Mage::getStoreConfig('bilna_paymentconfirmation/paymentconfirmation/sender_name'));
     $mailTo = explode(",",Mage::getStoreConfig('bilna_paymentconfirmation/paymentconfirmation/receiver_email'));
     $mail->addTo($mailTo);
-    $mail->setSubject('[BILNA] Payment Confirmation List '.Mage::getModel('core/date')->date('Y-m-d H', strtotime("-1 hours")));
+    $mail->setSubject('[BILNA] Payment Confirmation List '.Mage::getStoreConfig('bilna_paymentconfirmation/paymentconfirmation/next_execute').' To '.Mage::getModel('core/date')->date('Y-m-d H', strtotime("-1 hours")));
     $dir = Mage::getBaseDir();
     $file = $mail->createAttachment(file_get_contents($filename));
     $file ->type        = 'text/csv';
     $file ->disposition = Zend_Mime::DISPOSITION_INLINE;
     $file ->encoding    = Zend_Mime::ENCODING_BASE64;
-    $file ->filename    = sprintf('payment_confirmation_list_%s.csv',Mage::getModel('core/date')->date('YmdH', strtotime("-1 hours")));
+    $file ->filename    = sprintf('payment_confirmation_list_%s_%s.csv',  str_replace(array(" ","-"), "",Mage::getStoreConfig('bilna_paymentconfirmation/paymentconfirmation/next_execute')),Mage::getModel('core/date')->date('YmdH', strtotime("-1 hours")));
     $exitStatus = 0;
     try{
         $mail->send();
@@ -78,5 +95,6 @@
         $exitStatus = 1;
     }
     @unlink($filename);
+    Mage::getConfig()->saveConfig('bilna_paymentconfirmation/paymentconfirmation/next_execute',Mage::getModel('core/date')->date('Y-m-d H'));
     exit($exitStatus);
     
