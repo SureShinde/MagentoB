196ecc9a8e000c5b3195438ded3190e8e9c90921
M. Khairul Azami, S. Kom <khairul.azami@wgs.co.id>
2016-03-22 16:16:22 +0700

    add update collection profile api

---
+++ app/code/local/Bilna/Customer/Model/Api2/Wishlistcollectionprofile.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Description of Bilna_Customer_Model_Api2_Wishlistcollectionprofile
+ *
+ * @author Bilna Development Team <development@bilna.com>
+ */
+class Bilna_Customer_Model_Api2_Wishlistcollectionprofile extends Mage_Api2_Model_Resource
+{
+    public function _construct()
+    {
+        Mage::app()->getStore()->setStoreId(1);
+    }
+    
+    /**
+     * Load customer by id
+     *
+     * @param int $id
+     * @throws Mage_Api2_Exception
+     * @return Mage_Customer_Model_Customer
+     */
+    protected function _loadCustomerById($id)
+    {
+        /* @var $customer Mage_Customer_Model_Customer */
+        
+        $customer = Mage::getModel('customer/customer')->load($id);
+        
+        if (!$customer->getId()) {
+            $this->_critical(self::RESOURCE_NOT_FOUND);
+        }
+        
+        return $customer;
+    }
+}
---
+++ app/code/local/Bilna/Customer/Model/Api2/Wishlistcollectionprofile/Rest.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Description of Bilna_Customer_Model_Api2_Wishlistcollectionprofile_Rest
+ *
+ * @author Bilna Development Team <development@bilna.com>
+ */
+abstract class Bilna_Customer_Model_Api2_Wishlistcollectionprofile_Rest extends Bilna_Customer_Model_Api2_Wishlistcollectionprofile
+{
+}
