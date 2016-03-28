2152835d4c2ed10cd25d152226c64d50dff161be
M. KHAIRUL AZAMI, S. KOM <khairul.azami@bilna.com>
2016-03-28 16:40:40 +0700

    move total record

--- app/code/local/Bilna/Customer/Model/Api2/Wishlistcollection.php
+++ app/code/local/Bilna/Customer/Model/Api2/Wishlistcollection.php
@@ -66,14 +66,13 @@ class Bilna_Customer_Model_Api2_Wishlistcollection extends Bilna_Rest_Model_Api2
             # Get wishlist collection
             $wishlistCollection = Mage::getModel('wishlist/wishlist')->getCollection();
             $wishlistCollection->addFieldToFilter('customer_id', $customer->getId()); 
-            $wishlistCollection->setOrder('created_at', 'desc');
+            $wishlistCollection->setOrder('created_at', 'desc');                
             $result[0]['total_record'] = $wishlistCollection->getSize();
-
             $this->_pagination($wishlistCollection);
-
             $hasCollection = $wishlistCollection->count() < 1 ? false : true;
             
-            if ($wishlistCollection->getData() && $hasCollection) {                
+            if ($wishlistCollection->getData() && $hasCollection) {
+
                 foreach($wishlistCollection as $key => $value) {
                     $result[$key] = $value->getData();
                     $result[$key]['slug'] = $value->getId().'-'.Mage::getModel('catalog/product_url')->formatUrlKey($value->getName());
