2bdb2cc03d0a1050a64d7203103f2fab736a6a96
halimindra <indra.halim@bilna.com>
2016-04-18 12:07:09 +0700

    ORAMI-158: add additional element to display information of cross border product

--- app/code/local/Bilna/Crossborder/Helper/Data.php
+++ app/code/local/Bilna/Crossborder/Helper/Data.php
@@ -15,4 +15,17 @@ class Bilna_Crossborder_Helper_Data extends Mage_Core_Helper_Abstract {
         $crossBorderModel = Mage::getModel('bilna_crossborder/CrossBorder');
         return $crossBorderModel->hasCrossBorderItem();
     }
+
+    /**
+     * Helper to get html element for cross border product
+     * @param $product
+     * @return string
+     */
+    public function getHtmlCrossBorder($product)
+    {
+        if ($product->getData('cross_border') == 1) {
+            return '<span>import</span>';
+        }
+        return '<span>&nbsp;</span>';
+    }
 }
