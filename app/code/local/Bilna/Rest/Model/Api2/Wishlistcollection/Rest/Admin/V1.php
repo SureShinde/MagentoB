02b6af4ff1d6b97f449818ecbdac480f4c5aa7b7
M. KHAIRUL AZAMI, S. KOM <khairul.azami@bilna.com>
2016-04-19 12:35:39 +0700

    bug fix pagination public collection API

--- app/code/local/Bilna/Rest/Model/Api2/Wishlistcollection/Rest/Admin/V1.php
+++ app/code/local/Bilna/Rest/Model/Api2/Wishlistcollection/Rest/Admin/V1.php
@@ -21,40 +21,44 @@ class Bilna_Rest_Model_Api2_Wishlistcollection_Rest_Admin_V1 extends Bilna_Rest_
     protected function _getCollectionForRetrieve() 
     {
         try {
-            
             $wishlist_collection = [];
             $wishlist_collection_fav = [];
-            
+
             //get all wishlist collection
-            $wishlists = Mage::getModel('wishlist/wishlist')->getCollection();
-            $wishlists = $wishlists->addFilter('visibility', 1)
+            $wishlists = Mage::getModel('wishlist/wishlist')
+                ->getCollection()
+                ->addFilter('visibility', 1)
                 ->addFieldToFilter('name', array('notnull' => true))
                 ->addFieldToFilter('name', array('neq' => ' '))
                 ->addFieldToFilter('cover', array('notnull' => true))
                 ->setOrder('updated_at', 'desc');
             
+            $wishlist_collection[0]['total_record'] = $wishlists->getSize();
+            
+            $this->_pagination($wishlists);
+            
+            $wishlists->getSelect()
+            ->joinInner(
+                array('wishlist_item'=> Mage::getSingleton('core/resource')->getTableName('wishlist/item')),
+                'main_table.wishlist_id = wishlist_item.wishlist_id'
+            )
+            ->group('main_table.wishlist_id');
+
             if ($wishlists) {
-                $this->_pagination($wishlists);
-                
-                $counting = 0;
                 foreach($wishlists as $wishlist) {
-                    if($this->filterWishlistCollectionOutput($wishlist)) {
-                        $counting++;
-                        $wishlist_collection[$wishlist->getId()] = $wishlist->getData();
-                        $wishlist_collection[$wishlist->getId()]['slug'] = $wishlist->getId().'-'.Mage::getModel('catalog/product_url')->formatUrlKey($wishlist->getName());
-                        $wishlist_collection[$wishlist->getId()]['wishlist_collection_items_total'] = $this->getCollectionItemsTotal($wishlist);
-                        $profiler = Mage::getModel('socialcommerce/profile')->load($wishlist->getCustomerId(), 'customer_id');
-                        $wishlist_collection[$wishlist->getId()]['username'] = $profiler->getUsername();
-                    }
+                    $wishlist_collection[$wishlist->getId()] = $wishlist->getData();
+                    $wishlist_collection[$wishlist->getId()]['slug'] = $wishlist->getId().'-'.Mage::getModel('catalog/product_url')->formatUrlKey($wishlist->getName());
+                    $wishlist_collection[$wishlist->getId()]['wishlist_collection_items_total'] = $this->getCollectionItemsTotal($wishlist);
+                    $profiler = Mage::getModel('socialcommerce/profile')->load($wishlist->getCustomerId(), 'customer_id');
+                    $wishlist_collection[$wishlist->getId()]['username'] = $profiler->getUsername();
                 }
-                
-                $wishlist_collection[0]['total_record'] = $counting;
-                
+                //$wishlist_collection[1]['total_record_filter'] = (count($wishlist_collection) - 1);
             }
             
             //get all favourite wishlist collection
-            $faveWishlists = Mage::getModel('wishlist/wishlist')->getCollection();
-            $faveWishlists = $faveWishlists->addFilter('visibility', 1)
+            $faveWishlists = Mage::getModel('wishlist/wishlist')
+                ->getCollection()
+                ->addFilter('visibility', 1)
                 ->addFieldToFilter('name', array('notnull' => true))
                 ->addFieldToFilter('name', array('neq' => ' '))
                 ->addFieldToFilter('cover', array('notnull' => true))
@@ -62,18 +66,14 @@ class Bilna_Rest_Model_Api2_Wishlistcollection_Rest_Admin_V1 extends Bilna_Rest_
                 ->setPageSize(4);
             
             if ($faveWishlists) {
-                $faveCounting = 0;
-                foreach ($faveWishlists as $faveWishlist) {
-                    if($this->filterWishlistCollectionOutput($faveWishlist)) {
-                        $faveCounting++;
-                        $wishlist_collection_fav[$faveWishlist->getId()] = $faveWishlist->getData();
-                        $wishlist_collection_fav[$faveWishlist->getId()]['slug'] = $faveWishlist->getId().'-'.Mage::getModel('catalog/product_url')->formatUrlKey($faveWishlist->getName());
-                        $profiler = Mage::getModel('socialcommerce/profile')->load($faveWishlist->getCustomerId(), 'customer_id');
-                        $wishlist_collection_fav[$faveWishlist->getId()]['username'] = $profiler->getUsername();
-                    }
-                }
                 
-                $wishlist_collection_fav[0]['total_record'] = $faveCounting;
+                $wishlist_collection_fav[0]['total_record'] = $faveWishlists->getSize();
+                foreach ($faveWishlists as $faveWishlist) {
+                    $wishlist_collection_fav[$faveWishlist->getId()] = $faveWishlist->getData();
+                    $wishlist_collection_fav[$faveWishlist->getId()]['slug'] = $faveWishlist->getId().'-'.Mage::getModel('catalog/product_url')->formatUrlKey($faveWishlist->getName());
+                    $profiler = Mage::getModel('socialcommerce/profile')->load($faveWishlist->getCustomerId(), 'customer_id');
+                    $wishlist_collection_fav[$faveWishlist->getId()]['username'] = $profiler->getUsername();
+                }
             }
                 
             return [
@@ -85,20 +85,20 @@ class Bilna_Rest_Model_Api2_Wishlistcollection_Rest_Admin_V1 extends Bilna_Rest_
         }
     }
     
-    protected function _pagination($object)
+    protected function _pagination($object, $defaultPageSize = 24, $defaultCurPage = 1)
     {
         $limit = (int)$this->getRequest()->getParam('limit');
         $page = (int)$this->getRequest()->getParam('page');
-
-        if ($limit) {
+        
+        if ($limit > 0) {
             $object->setPageSize($limit);
         } else {
-            $object->setPageSize(10);
+            $object->setPageSize($defaultPageSize);
         }
-        if ($page) {
+        if ($page > 0) {
             $object->setCurPage($page);
         } else {
-            $object->setCurPage(1);
+            $object->setCurPage($defaultCurPage);
         }
     }
     
