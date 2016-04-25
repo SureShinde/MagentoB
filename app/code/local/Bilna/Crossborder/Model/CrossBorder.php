b4f1ae689cceddcc9cad477aceea0c89d35c16ed
halimindra <indra.halim@bilna.com>
2016-04-25 16:36:36 +0700

    ORAMI-159: refactor validation code to be reusable

--- app/code/local/Bilna/Crossborder/Model/CrossBorder.php
+++ app/code/local/Bilna/Crossborder/Model/CrossBorder.php
@@ -10,12 +10,39 @@ class Bilna_Crossborder_Model_CrossBorder
 {
 
     /**
-     * Function to validate cross border items based on configuration limit (weight, volume, quantity, and subtotal)
+     * Observer to validate cross border items based on configuration limit (weight, volume, quantity, and subtotal)
      * @param Varien_Event_Observer $observer
      * @return $this
      */
     public function validateSaveOrder(Varien_Event_Observer $observer)
     {
+        $order = $observer->getEvent()->getOrder();
+        $payment = $order->getPayment();
+        $paymentMethod = $payment->getMethod();
+
+        // Check if the 3D secure is enabled for that payment method
+        $threeDSecure = Mage::getStoreConfig('payment/' . $paymentMethod . '/threedsecure');
+
+        // If the payment don't have 3D secure or 3D secure is not enabled
+        if (is_null($threeDSecure) || $threeDSecure != "1") {
+            $validationResult = $this->validate();
+            if (!$validationResult['success']) {
+                Mage::throwException($validationResult['message']);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * Function to validate cross border items based on configuration limit (weight, volume, quantity, and subtotal)
+     * @return array
+     */
+    public function validate()
+    {
+        $success = true;
+        $message = '';
+
         $crossBorderHelper = Mage::helper('bilna_crossborder');
         if ($crossBorderHelper->isCrossBorderEnabled()) {
             $invalidCount = 0;
@@ -29,7 +56,7 @@ class Bilna_Crossborder_Model_CrossBorder
             $maxSubtotalAllowed = $crossBorderConfig['max_subtotal_allowed'];
 
             // Get All Cross Border Items and calculate the totals
-            $cartItems = Mage::getModel("checkout/cart")->getItems();
+            $cartItems = Mage::helper("checkout/cart")->getQuote()->getAllItems();
             foreach ($cartItems as $item) {
                 if ($item->cross_border == 1) {
                     $totalWeight += $item->weight * $item->qty;
@@ -58,10 +85,11 @@ class Bilna_Crossborder_Model_CrossBorder
             }
 
             if ($invalidCount > 0) { // If there is any invalid criteria, throw the Exception
-                Mage::throwException(Mage::helper('checkout')->__('CrossBorder:' . implode(', ', $messages)));
+                $message = Mage::helper('checkout')->__('CrossBorder:' . implode(', ', $messages));
+                $success = false;
             }
         }
 
-        return $this;
+        return array('success' => $success, 'message' => $message);
     }
 }
