622a4441262568ac591dc1aaea264eb1fb37d243
halimindra <indra.halim@bilna.com>
2016-04-27 11:22:12 +0700

    VIP-SHIPMENT: add try catch to ALTER TABLE query

--- app/code/local/Bilna/Expressshipping/sql/expressshipping_setup/mysql4-upgrade-0.1.1-0.1.2.php
+++ app/code/local/Bilna/Expressshipping/sql/expressshipping_setup/mysql4-upgrade-0.1.1-0.1.2.php
@@ -3,13 +3,17 @@
 $installer = $this;
 $installer->startSetup();
 
-$installer->run(
-	"CREATE TABLE `sales_order_daily_count` (
-		`id` int(11) NOT NULL AUTO_INCREMENT,
-		`sales_date` DATE,
-		`sales_count` INT,
-		PRIMARY KEY (`id`)
-	) ENGINE=InnoDB"
-);
+try {
+    $installer->run(
+        "CREATE TABLE `sales_order_daily_count` (
+            `id` int(11) NOT NULL AUTO_INCREMENT,
+            `sales_date` DATE,
+            `sales_count` INT,
+            PRIMARY KEY (`id`)
+        ) ENGINE=InnoDB"
+    );
+} catch (Exception $e) {
+    Mage::logException($e);
+}
 
 $installer->endSetup();
