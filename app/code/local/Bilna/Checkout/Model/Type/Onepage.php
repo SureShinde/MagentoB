d2657318d147bb4b88183fd3b4d9b08da306119d
harfil <harfil@bilna.com>
2016-05-25 09:38:02 +0700

    changing remark

--- app/code/local/Bilna/Checkout/Model/Type/Onepage.php
+++ app/code/local/Bilna/Checkout/Model/Type/Onepage.php
@@ -1,27 +1,8 @@
 <?php
 /**
- * Magento
+ * Description of Bilna_Checkout_Helper_Data
  *
- * NOTICE OF LICENSE
- *
- * This source file is subject to the Open Software License (OSL 3.0)
- * that is bundled with this package in the file LICENSE.txt.
- * It is also available through the world-wide-web at this URL:
- * http://opensource.org/licenses/osl-3.0.php
- * If you did not receive a copy of the license and are unable to
- * obtain it through the world-wide-web, please send an email
- * to license@magentocommerce.com so we can send you a copy immediately.
- *
- * DISCLAIMER
- *
- * Do not edit or add to this file if you wish to upgrade Magento to newer
- * versions in the future. If you wish to customize Magento for your
- * needs please refer to http://www.magentocommerce.com for more information.
- *
- * @category    Mage
- * @package     Mage_Checkout
- * @copyright   Copyright (c) 2013 Magento Inc. (http://www.magentocommerce.com)
- * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * @author Bilna Development Team <development@bilna.com>
  */
 
 /**
@@ -125,7 +106,7 @@ class Bilna_Checkout_Model_Type_Onepage extends Mage_Checkout_Model_Type_Onepage
     }
 
     /**
-     * This function will prevent raising condition if different user with same unique coupon code
+     * This function will prevent racing condition if different user with same unique coupon code
      * checkout at the same time (prevent single use coupon to be used multiple times at once)
      *
      */
@@ -151,7 +132,7 @@ class Bilna_Checkout_Model_Type_Onepage extends Mage_Checkout_Model_Type_Onepage
                 } catch (Exception $e) {
                     $errorMessage = $e->getMessage();
 
-                    // This is how we prevent raising condition by utilizing database unique lock
+                    // This is how we prevent racing condition by utilizing database unique lock
                     if ($errorMessage == "SQLSTATE[23000]: Integrity constraint violation: 1062 Duplicate entry '".$couponCode."' for key 'coupon_code'") {
                         $appliedRuleIds = $this->getQuote()->getAppliedRuleIds();
                         $appliedRuleIdsArray = explode(',', $appliedRuleIds);
