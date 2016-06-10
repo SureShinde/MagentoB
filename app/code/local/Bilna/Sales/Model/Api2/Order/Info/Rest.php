9c54ea2cfa0173b63136882dd627b3afb1cfbdae
Dhany Noor Alfian <dhany@bilna.com>
2016-06-10 11:03:31 +0700

    LOG-534 Order yang terdeteksi ADS statusnya berubah menjadi Pending VA setelah canceled - add response additional info for specific payment method

--- app/code/local/Bilna/Sales/Model/Api2/Order/Info/Rest.php
+++ app/code/local/Bilna/Sales/Model/Api2/Order/Info/Rest.php
@@ -7,6 +7,9 @@
  */
 
 abstract class Bilna_Sales_Model_Api2_Order_Info_Rest extends Bilna_Sales_Model_Api2_Order_Info {
+    protected $_order;
+    protected $_helper;
+    
     protected function _getOrder($orderId) {
         return Mage::getModel('sales/order')->load($orderId);
     }
@@ -15,11 +18,18 @@ abstract class Bilna_Sales_Model_Api2_Order_Info_Rest extends Bilna_Sales_Model_
         $statusCode = $order->getStatus();
         $status = Mage::getModel('sales/order_status')->load($statusCode);
         
-        return $status->getLabel();
+        return [
+            'code' => $statusCode,
+            'label' => $status->getLabel(),
+        ];
+    }
+    
+    protected function _isCanceled($orderState) {
+        return ($orderState === Mage_Sales_Model_Order::STATE_CANCELED);
     }
 
     protected function _getPaymentInfo($paymentMethod) {
-        return true;
+        return '';
     }
 
     protected function _isCreditCard($helper, $paymentMethod) {
@@ -47,8 +57,64 @@ abstract class Bilna_Sales_Model_Api2_Order_Info_Rest extends Bilna_Sales_Model_
     protected function _getAdditionalInfo($helper, $order, $paymentMethod) {
         $result = [];
         
-        if (in_array($paymentMethod, $helper->getPaymentMethodVA())) {
+        if (in_array($paymentMethod, $helper->getPaymentMethodKlikpay())) {
+            $additionalInfo = $this->_getAdditionalInfoKlikpay($order);
+            $result['data'] = $additionalInfo['data'];
+            $result['action'] = $additionalInfo['action']; 
+        }
+        elseif (in_array($paymentMethod, $helper->getPaymentMethodVA())) {
             $result['va_number'] = $order->getPayment()->getVaNumber();
+        }
+        elseif (in_array($paymentMethod, $helper->getPaymentMethodVtdirect())) {
+            $result['redirect_url'] = $this->_getAdditionalInfoVtdirect($order);
+        }
+        
+        return $result;
+    }
+    
+    protected function _getAdditionalInfoKlikpay($order) {
+        $klikpayConfig = Mage::getStoreConfig('payment/klikpay');
+        $klikpayUserId = $klikpayConfig['klikpay_user_id'];
+    	$transactionNo = $order->getIncrementId();
+    	$currency = 'IDR';
+    	$transactionAmount = number_format((int) $order->getGrandTotal(), 2, null, '');
+    	$payType = $order->getPayType();
+    	$callBackUrl = Mage::getBaseUrl() . 'success/' . $transactionNo;
+    	$transactionDate = date('d/m/Y H:i:s', strtotime($order->getCreatedAt()));
+    	$clearKey = $klikpayConfig['klikpay_clearkey'];
+    	$signature = Mage::helper('paymethod/klikpay')->signature($klikpayUserId, $transactionNo, $currency, $clearKey, $transactionDate, $transactionAmount);
+        $order->setKlikpaySignature(abs($signature))->save();
+        
+        return [
+            'data' => [
+                'klikPayCode' => $klikpayUserId,
+                'transactionNo' => $transactionNo,
+                'totalAmount' => $transactionAmount,
+                'currency' => $currency,
+                'payType' => $payType,
+                'callback' => $callBackUrl,
+                'transactionDate' => $transactionDate,
+                'descp' => '',
+                'miscFee' => '',
+                'signature' => abs($signature),
+            ],
+            'action' => $klikpayConfig['klikpay_redirect'],
+        ];
+    }
+    
+    protected function _getAdditionalInfoVtdirect($order) {
+        $incrementId = $order->getIncrementId();
+        $dbResource = Mage::getSingleton('core/resource');
+        $dbRead = $dbResource->getConnection('core_read');
+        $sql = "SELECT response FROM `veritrans_api_log` WHERE order_no = '{$incrementId}' AND type = 'C' LIMIT 1";
+        $result = '';
+        
+        if ($row = $dbRead->fetchOne($sql)) {
+            $response = json_decode($row, true);
+            
+            if ($redirectUrl = $response['redirect_url']) {
+                $result = $redirectUrl;
+            }
         }
         
         return $result;
