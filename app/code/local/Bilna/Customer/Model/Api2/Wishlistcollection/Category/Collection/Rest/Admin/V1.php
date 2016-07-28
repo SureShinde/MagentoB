4e3ada71dfed0b662024d8923a971756e61d93f1
Dhany Noor Alfian <dhanyalvian@gmail.com>
2016-07-28 21:35:07 +0700

    ORAMI-225 Collections can be separated into categories - new endpoint for Collection Category List Collection

---
+++ app/code/local/Bilna/Customer/Model/Api2/Wishlistcollection/Category/Collection/Rest/Admin/V1.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Description of Bilna_Customer_Model_Api2_Wishlistcollection_Category_Collection_Rest_Admin_V1
+ *
+ * @path    app/code/local/Bilna/Customer/Model/Api2/Wishlistcollection/Category/Collection/Rest/Admin/V1.php
+ * @author  Bilna Development Team <development@bilna.com>
+ */
+
+class Bilna_Customer_Model_Api2_Wishlistcollection_Category_Collection_Rest_Admin_V1 extends Bilna_Customer_Model_Api2_Wishlistcollection_Category_Collection_Rest {
+    protected function _retrieveCollection() {
+        
+    }
+}
