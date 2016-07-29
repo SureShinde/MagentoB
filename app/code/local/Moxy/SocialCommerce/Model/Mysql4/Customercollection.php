75b0de0b2a5775562cea81e5436babe015f5159a
harfil <harfil@bilna.com>
2016-07-29 19:52:36 +0700

    modify table and model for customer collection category

--- app/code/local/Moxy/SocialCommerce/Model/Mysql4/Customercollection.php
+++ app/code/local/Moxy/SocialCommerce/Model/Mysql4/Customercollection.php
@@ -3,6 +3,6 @@ class Moxy_SocialCommerce_Model_Mysql4_Customercollection extends Mage_Core_Mode
 {
     protected function _construct()
     {
-        $this->_init("socialcommerce/customercollection", "wishlist_id");
+        $this->_init("socialcommerce/customercollection", "map_id");
     }
 }
