aa622420d33c5dcf948a107415c197eb4459d266
Dhany Noor Alfian <dhany@bilna.com>
2016-07-29 18:13:40 +0700

    ORAMI-225 Collections can be separated into categories - change response

--- app/code/local/Bilna/Customer/Model/Api2/Wishlistcollection/Category.php
+++ app/code/local/Bilna/Customer/Model/Api2/Wishlistcollection/Category.php
@@ -7,5 +7,17 @@
  */
 
 class Bilna_Customer_Model_Api2_Wishlistcollection_Category extends Bilna_Customer_Model_Api2_Wishlistcollection {
-    
+    protected function _getPathByName($name) {
+        $search = [' '];
+        $replace = ['-'];
+
+        return strtolower(str_replace($search, $replace, $name));
+    }
+
+    protected function _getNameByPath($path) {
+        $search = ['-'];
+        $replace = [' '];
+
+        return strtolower(str_replace($search, $replace, $path));
+    }
 }
--- app/code/local/Bilna/Customer/Model/Api2/Wishlistcollection/Category/Collection/Rest/Admin/V1.php
+++ app/code/local/Bilna/Customer/Model/Api2/Wishlistcollection/Category/Collection/Rest/Admin/V1.php
@@ -8,6 +8,15 @@
 
 class Bilna_Customer_Model_Api2_Wishlistcollection_Category_Collection_Rest_Admin_V1 extends Bilna_Customer_Model_Api2_Wishlistcollection_Category_Collection_Rest {
     protected function _retrieveCollection() {
-        
+        $path = $this->getRequest()->getParam('path');
+        $categoryId = $this->_getCategoryIdByPath($path);
+    }
+
+    protected function _getCategoryIdByPath($path) {
+        $name = $this->_getNameByPath($path);
+        $collection = Mage::getModel('socialcommerce/collectioncategory')->getCollection();
+        $collection->addFieldToSelect('category_id');
+        $collection->getSelect()->where("LOWER(`name`) = '{$name}'");
+        $collection->printLogQuery(true);exit;
     }
 }
