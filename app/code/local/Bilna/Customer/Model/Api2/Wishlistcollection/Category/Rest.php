2d9721328c2b86378286d2c6fec9b1c7a6cf16cf
Dhany Noor Alfian <dhany@bilna.com>
2016-08-02 17:16:53 +0700

    ORAMI-225 Collections can be separated into categories - fix detail collection category

--- app/code/local/Bilna/Customer/Model/Api2/Wishlistcollection/Category/Rest.php
+++ app/code/local/Bilna/Customer/Model/Api2/Wishlistcollection/Category/Rest.php
@@ -10,7 +10,6 @@ abstract class Bilna_Customer_Model_Api2_Wishlistcollection_Category_Rest extend
     protected function _getCategoryCollection($urlKey = '') {
         $collection = Mage::getModel('socialcommerce/collectioncategory')->getCollection();
         $collection->addFieldToFilter('is_active', 1);
-        $collection->addFieldToFilter('show_in_coll_page', 1);
         $collection->getSelect()->order('sort_no', 'ASC');
 
         if ($urlKey) {
@@ -19,6 +18,7 @@ abstract class Bilna_Customer_Model_Api2_Wishlistcollection_Category_Rest extend
             return $collection->getFirstItem();
         }
         else {
+            $collection->addFieldToFilter('show_in_coll_page', 1);
             $collection->load();
 
             return $collection;
