499d209544768daaa699a8c09245df62fd53aff2
harfil <harfil@bilna.com>
2016-08-16 11:25:23 +0700

    change way to sanitize url

--- app/code/local/Moxy/SocialCommerce/Helper/Data.php
+++ app/code/local/Moxy/SocialCommerce/Helper/Data.php
@@ -432,12 +432,24 @@ class Moxy_SocialCommerce_Helper_Data extends Mage_Core_Helper_Abstract
     {
         //Lower case everything
         $string = strtolower($string);
-        //Make alphanumeric (removes all other characters)
-        $string = preg_replace("/[^a-z0-9_\s-]/", "", $string);
-        //Clean up multiple dashes or whitespaces
-        $string = preg_replace("/[\s-]+/", " ", $string);
-        //Convert whitespaces and underscore to dash
-        $string = preg_replace("/[\s_]/", "-", $string);
+        /**
+         * Make alphanumeric (removes all other characters)
+         * Clean up multiple dashes or whitespaces
+         * Convert whitespaces and underscore to dash
+         */
+        $patterns = array(
+            '/[^a-z0-9_\s-]/',
+            '/[\s-]+/',
+            '/[\s_]/'
+        );
+
+        $replace = array(
+            '',
+            ' ',
+            '-'
+        );
+
+        $string = preg_replace($patterns, $replace, $string);
         return $string;
     }
 }
