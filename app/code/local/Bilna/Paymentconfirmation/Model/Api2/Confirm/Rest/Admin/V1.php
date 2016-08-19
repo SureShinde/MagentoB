200ffdf8514d944a632db7c093883647acd262a6
dendhi31 <dendhi31@yahoo.com>
2016-08-19 14:32:16 +0700

    API prevent double confirmation

--- app/code/local/Bilna/Paymentconfirmation/Model/Api2/Confirm/Rest/Admin/V1.php
+++ app/code/local/Bilna/Paymentconfirmation/Model/Api2/Confirm/Rest/Admin/V1.php
@@ -9,22 +9,30 @@
 class Bilna_Paymentconfirmation_Model_Api2_Confirm_Rest_Admin_V1 extends Bilna_Paymentconfirmation_Model_Api2_Confirm_Rest {
     protected function _create(array $data) {
         $this->_validate($data);
-        
+
         try {
             /* @var $validator Bilna_Paymentconfirmation_Model_Payment */
             $confirmModel = Mage::getModel('Paymentconfirmation/payment');
             $order = $confirmModel->isValidOrder($data['order_number']);
-            
+
             if (!$order->getId()) {
                 $this->_error('Order Number is not valid.', Mage_Api2_Model_Server::HTTP_BAD_REQUEST);
-                $this->_critical(self::RESOURCE_REQUEST_DATA_INVALID);
             }
-            
+
             if (!$this->_validateOrderEmail($order, $data['email'])) {
                 $this->_error('Customer Email is not valid.', Mage_Api2_Model_Server::HTTP_BAD_REQUEST);
-                $this->_critical(self::RESOURCE_REQUEST_DATA_INVALID);
             }
-            
+
+            $confirmData = $confirmModel
+                ->getCollection()
+                ->addFieldToFilter('order_id',array('equal' => $data['order_number']))
+                ->setCurPage(1)
+                ->setPageSize(1);
+
+            if(count($confirmData) > 0){
+                $this->_error('Order Number already confirmed.', Mage_Api2_Model_Server::HTTP_BAD_REQUEST);
+            }
+
             $fields = array (
                 'order_id' => $data['order_number'],
                 'email' => $data['email'],
@@ -38,18 +46,18 @@ class Bilna_Paymentconfirmation_Model_Api2_Confirm_Rest_Admin_V1 extends Bilna_P
             );
             $confirmModel->setData($fields);
             $confirmModel->save();
-            
+
             if (!$confirmModel->getId()) {
                 $this->_critical(self::RESOURCE_NOT_FOUND);
             }
-            
+
             return $this->_getLocation($confirmModel);
         }
         catch (Exception $e) {
             $this->_critical($e->getMessage());
         }
     }
-    
+
     protected function _validate($data) {
         /* @var $validator Bilna_Paymentconfirmation_Model_Api2_Validator_Confirm */
         $validator = Mage::getModel('Paymentconfirmation/api2_validator_confirm');
@@ -58,16 +66,16 @@ class Bilna_Paymentconfirmation_Model_Api2_Confirm_Rest_Admin_V1 extends Bilna_P
             foreach ($validator->getErrors() as $error) {
                 $this->_error($error, Mage_Api2_Model_Server::HTTP_BAD_REQUEST);
             }
-            
+
             $this->_critical(self::RESOURCE_DATA_PRE_VALIDATION_ERROR);
         }
     }
-    
+
     protected function _validateOrderEmail($order, $email) {
         if ($order->getCustomerEmail() != $email) {
             return false;
         }
-        
+
         return true;
     }
 }
