114593748cf5344d54dbad1308ea72de18185ffc
indrakbil <indra.kurniawan@bilna.com>
2016-09-03 15:06:39 +0700

    remove db name on mysql upgrade script

--- app/code/local/Bilna/Expressshipping/sql/expressshipping_setup/mysql4-upgrade-0.1.2-0.1.3.php
+++ app/code/local/Bilna/Expressshipping/sql/expressshipping_setup/mysql4-upgrade-0.1.2-0.1.3.php
@@ -5,7 +5,7 @@ $installer->startSetup();
 
 try {
 	$installer->run(" 
-		ALTER TABLE `bilna_live`.`sales_order_daily_count` 
+		ALTER TABLE `sales_order_daily_count` 
 		CHANGE COLUMN `sales_date` `sales_datetime_from` DATETIME NULL DEFAULT NULL ,
 		ADD COLUMN `sales_datetime_to` DATETIME NULL;
 		");
