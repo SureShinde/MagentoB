2d339d50231d81ddbbb0b9cceb8b2fae01ff032e
dendhi31 <dendhi31@yahoo.com>
2016-09-05 17:01:55 +0700

    add update credit notitication status api

---
+++ app/code/local/Bilna/Customer/Model/Api2/Creditnotification.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Description of Bilna_Customer_Model_Api2_Wishlistcollectionprofile
+ *
+ * @author Bilna Development Team <development@bilna.com>
+ */
+class Bilna_Customer_Model_Api2_Creditnotification extends Bilna_Rest_Model_Api2
+{
+    
+}
---
+++ app/code/local/Bilna/Customer/Model/Api2/Creditnotification/Rest.php
@@ -0,0 +1,12 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+abstract class Bilna_Customer_Model_Api2_Creditnotification_Rest extends Bilna_Customer_Model_Api2_Creditnotification
+{
+    
+}
+
