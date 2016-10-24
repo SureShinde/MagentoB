45e923c1c7b0e80618d05afcdad6ebe5fe774149
harfil <harfil@bilna.com>
2016-10-24 17:27:08 +0700

    remove unused setter

--- app/code/local/Bilna/Wholesale/Model/Carrier.php
+++ app/code/local/Bilna/Wholesale/Model/Carrier.php
@@ -1,5 +1,7 @@
 <?php
-class Bilna_Wholesale_Model_Carrier extends Mage_Shipping_Model_Carrier_Abstract implements Mage_Shipping_Model_Carrier_Interface
+class Bilna_Wholesale_Model_Carrier
+    extends Mage_Shipping_Model_Carrier_Abstract
+    implements Mage_Shipping_Model_Carrier_Interface
 {
     protected $_code = 'pickup';
 
@@ -12,12 +14,18 @@ class Bilna_Wholesale_Model_Carrier extends Mage_Shipping_Model_Carrier_Abstract
 
     public function collectRates(Mage_Shipping_Model_Rate_Request $request)
     {
-        if (Mage::getSingleton('checkout/session')->getQuote()->getIsWholesale()) {
-            $result = Mage::getModel('shipping/rate_result');
-            $result->append($this->_getPickupData());
-
-            return $result;
+        if (
+            !Mage::getSingleton('checkout/session')
+                ->getQuote()
+                ->getIsWholesale()
+        ) {
+            return;
         }
+
+        $result = Mage::getModel('shipping/rate_result');
+        $result->append($this->_getPickupData());
+
+        return $result;
     }
 
     protected function _getPickupData()
@@ -26,12 +34,10 @@ class Bilna_Wholesale_Model_Carrier extends Mage_Shipping_Model_Carrier_Abstract
 
         $rate->setCarrier($this->_code);
         $rate->setCarrierTitle($this->getConfigData('title'));
-        $rate->setMethod('large');
         $rate->setMethodTitle($this->getConfigData('name'));
-        $rate->setMethodDescription('test');
         $rate->setPrice($this->getConfigData('price'));
-        $rate->setCost($this->getConfigData('handling_fee'));
 
         return $rate;
     }
 }
+
