9a3edb42584cada397d386796b6833c7683224b4
harfil <harfil@bilna.com>
2016-10-25 19:06:30 +0700

    add checking to ensure that we have item(s) in $request object before processing further

--- app/code/local/Bilna/Wholesale/Model/Carrier.php
+++ app/code/local/Bilna/Wholesale/Model/Carrier.php
@@ -15,6 +15,11 @@ class Bilna_Wholesale_Model_Carrier
     public function collectRates(Mage_Shipping_Model_Rate_Request $request)
     {
         $allItems = $request->getAllItems();
+
+        if (count($allItems) <= 0) {
+            return;
+        }
+
         $isWholesale = $allItems[0]->getQuote()->getIsWholesale();
 
         if (!$isWholesale) {
