0d10d51f4e75451b1d8906ee258ca80cb6c6875a
dendhi31 <dendhi31@yahoo.com>
2016-11-03 18:03:27 +0700

    ORM-1171 Allow user to take over verified number from another user

--- app/code/local/Bilna/Smsverification/Model/Api2/Verify/Rest/Admin/V1.php
+++ app/code/local/Bilna/Smsverification/Model/Api2/Verify/Rest/Admin/V1.php
@@ -15,10 +15,24 @@ class Bilna_Smsverification_Model_Api2_Verify_Rest_Admin_V1 extends Bilna_Smsver
             ->addFilter('otp_code',array('equal' => $data['otp_code']))
             ->addFilter('customer_id',array('equal' => $data['customer_id']));
         if(count($OTPData) > 0) {
+            $otherCustomer = Mage::getModel('customer/customer')->getCollection()
+                            ->addAttributeToFilter('mobile_number',array('eq' => $data['msisdn']))
+                            ->addAttributeToFilter('entity_id',array('neq' => $data['customer_id']))
+                            ->getData();
+            if(count($otherCustomer) > 0) {
+                foreach($otherCustomer as $idx => $val) {
+                    $customer = Mage::getModel('customer/customer')->load($val['entity_id']);
+                    $customer->setMobileNumber(NULL);
+                    $customer->setVerifiedDate(NULL);
+                    $customer->save();
+                }
+            }
+
             $customer = Mage::getModel('customer/customer')->load($data['customer_id']);
             $customer->setMobileNumber($data['msisdn']);
             $customer->setVerifiedDate(Mage::getModel('core/date')->date('Y-m-d H:i:s'));
             $customer->save();
+
             return $OTPData->getFirstItem()->delete();
         } else {
             $this->_critical('Invalid OTP Code');
