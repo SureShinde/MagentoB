4908103fcb401fcccf076a05cf9fcb99ea4fc11f
dendhi31 <dendhi31@yahoo.com>
2016-11-08 19:04:00 +0700

    ORM-1163 verified otp will not deleted from table, it will change type to 1

--- app/code/local/Bilna/Smsverification/Model/Api2/Verify/Rest/Admin/V1.php
+++ app/code/local/Bilna/Smsverification/Model/Api2/Verify/Rest/Admin/V1.php
@@ -7,12 +7,13 @@
 class Bilna_Smsverification_Model_Api2_Verify_Rest_Admin_V1 extends Bilna_Smsverification_Model_Api2_Verify_Rest
 {
     protected function _create(array $data) {
-        $data['msisdn'] = substr($data['msisdn'],0,1) == "0" ? "62".substr($data['msisdn'],1) : $data['msisdn'];
+        //$data['msisdn'] = substr($data['msisdn'],0,1) == "0" ? "62".substr($data['msisdn'],1) : $data['msisdn'];
         $OTPModel = Mage::getModel('smsverification/otplist');
         $OTPData = $OTPModel
             ->getCollection()
             ->addFilter('msisdn',array('equal' => $data['msisdn']))
             ->addFilter('otp_code',array('equal' => $data['otp_code']))
+            ->addFilter('type',array('equal' => 0))
             ->addFilter('customer_id',array('equal' => $data['customer_id']));
         if(count($OTPData) > 0) {
             $OTP = $OTPData->getFirstItem()->getData();
@@ -23,14 +24,17 @@ class Bilna_Smsverification_Model_Api2_Verify_Rest_Admin_V1 extends Bilna_Smsver
                 $OTPData->getFirstItem()->delete();
                 $this->_critical('Expired OTP Code');
             }
-            if(substr($data['msisdn'],0,2) == "62") {
-                $data['msisdn'] = "0".substr($data['msisdn'],2);
-            }
+            //if(substr($data['msisdn'],0,2) == "62") {
+            //    $data['msisdn'] = "0".substr($data['msisdn'],2);
+            //}
             $customer = Mage::getModel('customer/customer')->load($data['customer_id']);
             $customer->setMobileNumber($data['msisdn']);
             $customer->setVerifiedDate(Mage::getModel('core/date')->date('Y-m-d H:i:s'));
             $customer->save();
-            return $OTPData->getFirstItem()->delete();
+            //return $OTPData->getFirstItem()->delete();
+            $data = $OTPData->getFirstItem();
+            $data->setType(1);
+            $data->save();
         } else {
             $this->_critical('Invalid OTP Code');
         }
