108b635e48a4fb5dafcccb12b528f8d12ba5def6
dendhi31 <dendhi31@yahoo.com>
2016-11-16 16:49:46 +0700

    ORM-1280 remove unused comment from observer

--- app/code/local/Bilna/Smsverification/Model/Observer.php
+++ app/code/local/Bilna/Smsverification/Model/Observer.php
@@ -7,27 +7,24 @@ class Bilna_Smsverification_Model_Observer extends Mage_Core_Block_Abstract
             return;
         }
         $order = $observer->getOrder();
-        //$paymentMethod = $order->getPayment()->getMethodInstance()->getTitle();
 
         $isCod = Mage::Helper('cod')->isCodOrder($order);
         if(!$isCod) return;
 
-        //if($paymentMethod == Mage::getStoreConfig('payment/cod/title')) {
-            $body = Mage::getStoreConfig('bilna/smsverification/template_trans');
-            $body = str_replace("[TRX]", $order->getIncrementId(), $body);
-            //Send SMS
-            $smsHelper = Mage::Helper('smsverification');
-            try{
-                $msisdn = $smsHelper->validateMobileNumber($order->getShippingAddress()->getTelephone());
-                $drID = $smsHelper->sendSMS($msisdn,$body);
-                $drModel = Mage::getModel('smsverification/smsdr');
-                $data = array('code' => $drID, 'order_id' => $order->getId(), 'msisdn' => $msisdn);//, 'created_at' => Mage::getModel('core/date')->date('Y-m-d H:i:s'));
-                $drModel->setData($data);
-                return $drModel->save();
-            } catch (Exception $e) {
-                throw new Exception($e->getMessage());
+        $body = Mage::getStoreConfig('bilna/smsverification/template_trans');
+        $body = str_replace("[TRX]", $order->getIncrementId(), $body);
+        //Send SMS
+        $smsHelper = Mage::Helper('smsverification');
+        try{
+            $msisdn = $smsHelper->validateMobileNumber($order->getShippingAddress()->getTelephone());
+            $drID = $smsHelper->sendSMS($msisdn,$body);
+            $drModel = Mage::getModel('smsverification/smsdr');
+            $data = array('code' => $drID, 'order_id' => $order->getId(), 'msisdn' => $msisdn);
+            $drModel->setData($data);
+            return $drModel->save();
+        } catch (Exception $e) {
+            throw new Exception($e->getMessage());
 
-            }
-        //}
+        }
     }
 }
