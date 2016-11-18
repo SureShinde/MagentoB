f110e333fe8ee4f230cf31acfd980746b618d2a9
dendhi31 <dendhi31@yahoo.com>
2016-11-18 11:46:27 +0700

    ORM-1281 add index controller te retrive DR from wavecell

--- app/code/local/Bilna/Smsverification/controllers/IndexController.php
+++ app/code/local/Bilna/Smsverification/controllers/IndexController.php
@@ -1,7 +1,7 @@
 <?php
 class Bilna_Smsverification_IndexController extends Mage_Core_Controller_Front_Action {
     public function IndexAction() {
-        print_r($_REQUEST);
+        Mage::log("Deni Wavecell Param: ".json_encode($_REQUEST));
     }
 
 }
