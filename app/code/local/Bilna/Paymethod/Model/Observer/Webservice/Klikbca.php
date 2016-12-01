f472c321816a97614385186434d835a22b408b1d
Kevin Putrajaya <kevin.putrajaya@bilna.com>
2016-12-01 15:18:33 +0700

    Move KlikBCA confirmation from file to queue

--- app/code/local/Bilna/Paymethod/Model/Observer/Webservice/Klikbca.php
+++ app/code/local/Bilna/Paymethod/Model/Observer/Webservice/Klikbca.php
@@ -10,40 +10,40 @@ class Bilna_Paymethod_Model_Observer_Webservice_Klikbca {
     protected $_currency = 'IDR';
     protected $_formatDate = 'Y-m-d H:i:s';
     protected $_formatDate2 = 'm/d/Y H:i:s';
-    
+
     protected $_statusTrxSuccess = '00';
     protected $_statusTrxFailed = '01';
-    
+
     protected $_reasonTrxSuccess = 'Success.';
     protected $_reasonTrxFailed = 'Your transaction cannot be processed.';
     protected $_reasonTrxPaid = 'Your transaction has been paid.';
     protected $_reasonTrxExpired = 'Your transaction has expired.';
-    
+
     protected $_typeTransaction = 'transaction';
     protected $_typeConfirmation = 'confirmation';
-    
+
     public function inquiryAction() {
         $this->writeLog($this->_typeTransaction, 'inquiry', 'request_method: ' . $this->getRequestMethod());
-        
+
         $klikbcaUserId = $this->getRequestData('userid');
         $additionalData = $this->getRequestData('adddata');
-        
+
         $contentLog = sprintf("%s | request_klikbca: %s", $klikbcaUserId, json_encode($this->getRequestData()));
         $this->writeLog($this->_typeTransaction, 'inquiry', $contentLog);
-        
+
         $validationHours = Mage::getStoreConfig('payment/klikbca/order_validation');
         $validationOption = Mage::getStoreConfig('payment/klikbca/order_validation_option');
-        
+
         $orderCollection = Mage::getModel('sales/order')->getCollection();
         $orderCollection->addFieldToFilter('status', 'pending');
         $orderCollection->getSelect()->join(array ('SFOP' => 'sales_flat_order_payment'), 'klikbca_user_id="' . $klikbcaUserId . '" AND SFOP.method="' . $this->_code . '" AND main_table.entity_id=SFOP.parent_id', array ('klikbca_user_id'), null, 'left');
-        
+
         header('Content-type: text/xml; charset=utf-8');
         $xml  = '<?xml version="1.0" encoding="utf-8"?>';
         $xml .= '<OutputListTransactionPGW>';
         $xml .= "<userID>" . $klikbcaUserId . "</userID>";
         $xml .= "<additionalData>" . $additionalData . "</additionalData>";
-        
+
         foreach ($orderCollection as $order) {
             if ($validationHours) {
             	$orderCreatedDate = $order->getCreatedAt();
@@ -54,10 +54,10 @@ class Bilna_Paymethod_Model_Observer_Webservice_Klikbca {
                     continue;
                 }
             }
-            
+
             $items = $order->getAllItems();
             $productName = '';
-            
+
             foreach ($items as $itemId => $item) {
                 $productName = $item->getName();
                 break;
@@ -68,19 +68,19 @@ class Bilna_Paymethod_Model_Observer_Webservice_Klikbca {
             $xml .= "<transactionDate>" . date($this->_formatDate2, strtotime($order->getCreatedAt())) . "</transactionDate>";
             $xml .= "<amount>" . $this->_currency . number_format((int) $order->getGrandTotal(), 2, null, '') . "</amount>";
             $xml .= "<description>" . Mage::helper('paymethod/klikbca')->_removeSymbols($productName) . "</description>";
-            $xml .= "</OutputDetailPayment>";                     
+            $xml .= "</OutputDetailPayment>";
         }
 
         $xml .= "</OutputListTransactionPGW>";
-        
+
         $contentLog = sprintf("%s | response_klikbca: %s", $_GET['userid'], $xml);
         $this->writeLog($this->_typeTransaction, 'inquiry', $contentLog);
         die ($xml);
     }
-    
+
     public function paymentAction() {
         $this->writeLog($this->_typeTransaction, 'payment', 'request_method: ' . $this->getRequestMethod());
-        
+
         $klikbcaUserId = $this->getRequestData('userid');
         $transactionNo = $this->getRequestData('transno');
         $transactionDate = $this->getRequestData('transdate');
@@ -90,14 +90,14 @@ class Bilna_Paymethod_Model_Observer_Webservice_Klikbca {
         $additionalData = $this->getRequestData('adddata');
         $reason = '';
         $status = '';
-        
+
         $contentLog = sprintf("%s | request_klikbca: %s", $klikbcaUserId, json_encode($this->getRequestData()));
         $this->writeLog($this->_typeTransaction, 'payment', $contentLog);
-        
+
         $orderCollection = Mage::getModel('sales/order')->getCollection();
         $orderCollection->addFieldToFilter('increment_id', array ('eq' => $transactionNo));
         $orderCollection->getSelect()->join(array ('SFOP' => 'sales_flat_order_payment'), 'klikbca_user_id = "' . $klikbcaUserId . '" AND SFOP.method = "' . $this->_code . '" AND main_table.entity_id = SFOP.parent_id', array ('klikbca_user_id'), null, 'left')->limit(1);
-        
+
         $order = $orderCollection->getFirstItem();
         $validationHours = Mage::getStoreConfig('payment/klikbca/order_validation');
         $validationOption = Mage::getStoreConfig('payment/klikbca/order_validation_option');
@@ -112,7 +112,7 @@ class Bilna_Paymethod_Model_Observer_Webservice_Klikbca {
                 $validHours = 'no';
             }
         }
-        
+
         if ($order->getId()) {
             if (number_format((int) $order->getGrandTotal(), 2, null, '') != $transactionAmount) {
                 $reason = $this->_reasonTrxFailed;
@@ -128,12 +128,20 @@ class Bilna_Paymethod_Model_Observer_Webservice_Klikbca {
             }
             else {
                 try {
-                   $order->setState(Mage_Sales_Model_Order::STATE_NEW, 'klikbca_pending', Mage::getStoreConfig('payment/klikbca/order_klikbca_pending_comment'), true)->save();
-                   $status = $this->_statusTrxSuccess;
-                   $reason = $this->_reasonTrxSuccess;
-                   
-                   $contentLog = sprintf("%s|%s|%s|%s|%s|%s", $klikbcaUserId, $transactionNo, $transactionDate, $transactionAmountCurrency, $type, $additionalData);
-                   $this->createLog($transactionNo, 'confirmation', $contentLog);
+                    $order->setState(Mage_Sales_Model_Order::STATE_NEW, 'klikbca_pending', Mage::getStoreConfig('payment/klikbca/order_klikbca_pending_comment'), true)->save();
+                    $status = $this->_statusTrxSuccess;
+                    $reason = $this->_reasonTrxSuccess;
+
+                    $data = [
+                        'userid' => $klikbcaUserId,
+                        'transno' => $transactionNo,
+                        'transdate' => $transactionDate,
+                        'amount' => $transactionAmountCurrency,
+                        'type' => $type,
+                        'adddata' => $additionalData
+                    ];
+                    Mage::helper('paymethod')->enqueueKlikbcaConfirmation($data);
+                    $this->createLog($transactionNo, 'confirmation', json_encode($data));
                 }
                 catch (Mage_Core_Exception $e) {
                     Mage::log($e->getMessage(), null, 'klikbca_error.log', true);
@@ -143,7 +151,7 @@ class Bilna_Paymethod_Model_Observer_Webservice_Klikbca {
         else {
             $reason = $this->_reasonTrxFailed;
         }
-        
+
         header('Content-type: text/xml; charset=utf-8');
         $xml  = '<?xml version="1.0" encoding="utf-8"?><OutputPaymentPGW>';
         $xml .= "<userID>" . $klikbcaUserId . "</userID>";
@@ -153,7 +161,7 @@ class Bilna_Paymethod_Model_Observer_Webservice_Klikbca {
         $xml .= "<reason>" . $reason . "</reason>";
         $xml .= "<additionalData>" . $additionalData . "</additionalData>";
         $xml .= "</OutputPaymentPGW>";
-        
+
         $contentLog = sprintf("%s | response_klikbca: %s", $klikbcaUserId, $xml);
         $this->writeLog($this->_typeTransaction, 'payment', $contentLog);
         die ($xml);
@@ -162,11 +170,11 @@ class Bilna_Paymethod_Model_Observer_Webservice_Klikbca {
     protected function getRequestMethod() {
         return Mage::getStoreConfig('payment/' . $this->_code . '/request_method');
     }
-    
+
     protected function getRequestData($key = '') {
         $method = $this->getRequestMethod();
         $result = '';
-        
+
         if ($method == 'POST') {
             if ($key == '') {
                 $result = $_POST;
@@ -187,14 +195,14 @@ class Bilna_Paymethod_Model_Observer_Webservice_Klikbca {
                 }
             }
         }
-        
+
         return $result;
     }
-    
+
     protected function getRequestDataReplace($key, $replace) {
         $method = Mage::getStoreConfig('payment/klikbca/request_method');
         $result = '';
-        
+
         if ($method == 'POST') {
             if ($_POST[$key]) {
                 $result = $_POST[$key];
@@ -213,21 +221,21 @@ class Bilna_Paymethod_Model_Observer_Webservice_Klikbca {
                 }
             }
         }
-        
+
         return $result;
     }
-    
+
     protected function writeLog($type, $logFile, $content) {
         $tdate = date('Ymd', Mage::getModel('core/date')->timestamp(time()));
         $filename = sprintf("%s_%s.%s", $this->_code, $logFile, $tdate);
-        
+
         return Mage::helper('paymethod')->writeLogFile($this->_code, $type, $filename, $content);
     }
-    
+
     protected function createLog($filename, $type, $content) {
         $tdate = date('Y-m-d H:i:s', Mage::getModel('core/date')->timestamp(time()));
         $content = sprintf("%s|%s", $content, $tdate);
-        
+
         return Mage::helper('paymethod')->writeLogFile($this->_code, $type, $filename, $content, 'normal');
     }
 }
