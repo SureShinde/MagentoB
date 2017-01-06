d44a510cec5284f6a1e5fd1a8fa2e94bf25525cb
M. KHAIRUL AZAMI, S. KOM <khairul.azami@bilna.com>
2017-01-06 18:23:08 +0700

    ORM-1589 Rapihkan Code BilnaAffiliate - fix magento API affiliate order complete - komisi tidak tersimpan jika paymethod mandiriecash, klikpay - campaign id tidak ditemukan - affiliate profile tidak ditemukan - attracted amount and amount is invalid

--- app/code/local/AW/Affiliate/Model/Api2/Ordercomplete/Rest/Admin/V1.php
+++ app/code/local/AW/Affiliate/Model/Api2/Ordercomplete/Rest/Admin/V1.php
@@ -20,6 +20,7 @@ class AW_Affiliate_Model_Api2_Ordercomplete_Rest_Admin_V1 extends AW_Affiliate_M
             $clientModel = Mage::getModel('awaffiliate/client')->load($client);
             $orderModel = Mage::getModel('sales/order')->load($order); //order id = entity if from sales_flat_order table
             $affiliateModel = Mage::getModel('awaffiliate/affiliate')->load($clientModel->getAffiliateId());
+            
             if (is_null($clientModel->getId())) {
                 $status = 'error';
                 $messages[] = Mage::helper('awaffiliate')->__('Unable to get the client ID');
@@ -45,32 +46,31 @@ class AW_Affiliate_Model_Api2_Ordercomplete_Rest_Admin_V1 extends AW_Affiliate_M
 
                 $campaign = $client->getCampaign();
                 $conditionsModel = $campaign->getConditionsModel();
-
+                
                 if ($orderModel->hasInvoices()) {
                     foreach ($orderModel->getInvoiceCollection() as $invoice) {
                         $getInvoice = $invoice;
                     }
                 }
-                if ($conditionsModel->getActions()->validate($order)) {
+                if ($conditionsModel->getActions()->validate($orderModel)) {
                     /** @var $trx AW_Affiliate_Model_Transaction_Profit */
-                    $trx = Mage::getModel('awaffiliate/transaction_profit');
-                    $trx->setData(array(
-                        'campaign_id' => $client->getData('campaign_id'),
-                        'affiliate_id' => $client->getData('affiliate_id'),
-                        'traffic_id' => $client->getData('traffic_id'),
-                        'client_id' => $client->getId(),
+                    $trx = Mage::getModel('awaffiliate/transaction_profit', array(
+                        'campaign_id' => $clientModel->getCampaignId(),
+                        'affiliate_id' => $clientModel->getAffiliateId(),
                         'linked_entity_type' => AW_Affiliate_Model_Source_Transaction_Profit_Linked::INVOICE_ITEM,
                         'linked_entity_id' => $orderModel->getIncrementId(),
+                        'store_id' => self::DEFAULT_STORE_ID,
                         'linked_entity_invoice' => $getInvoice,
                         'linked_entity_order' => $orderModel,
-                        'created_at' => Mage::getModel('core/date')->gmtDate(),
                         'type' => AW_Affiliate_Model_Source_Transaction_Profit_Type::CUSTOMER_PURCHASE
                     ));
+                    
                     $trx->createTransaction();
                 }
                 
                 $status = 'success';
                 $messages[] = Mage::helper('awaffiliate')->__('Affiliate client history saved successfully');
+                
             } catch (Exception $e) {
                 $status = 'error';
                 $messages[] = Mage::helper('awaffiliate')->__($e->getMessage());
